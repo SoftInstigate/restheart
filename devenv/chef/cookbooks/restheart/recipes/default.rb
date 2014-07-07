c8b7fe68d5056e39789b3831c2b8bd1fb08b441d
Andrea Di Cesare <andrea@softinstigate.com>
2014-07-07 17:43:26 +0200

    refactored to match the new project name "restheart"

--- devenv/chef/cookbooks/restheart/recipes/default.rb
+++ devenv/chef/cookbooks/restheart/recipes/default.rb
@@ -1,15 +1,15 @@
 execute "build" do
-	command "cd /restart; mvn package"
-	only_if { ::File.exists?("/restart/.git") }
+	command "cd /restheart; mvn package"
+	only_if { ::File.exists?("/restheart/.git") }
 end
 
-cookbook_file "restart.yml" do
-	path "/home/vagrant/restart-chef.yml" 
+cookbook_file "restheart.yml" do
+	path "/home/vagrant/restheart-chef.yml" 
 	mode 0644
 	owner "root"
 end
 
 execute "run" do
-	command "killall java; nohup java -server -jar /restart/target/RESTart-1.0-SNAPSHOT-jar-with-dependencies.jar /home/vagrant/restart-chef.yml &"
-	only_if { ::File.exists?("/restart/target/RESTart-1.0-SNAPSHOT-jar-with-dependencies.jar") }
+	command "killall java; nohup java -server -jar /restheart/target/restheart-1.0-SNAPSHOT-jar-with-dependencies.jar /home/vagrant/restheart-chef.yml &"
+	only_if { ::File.exists?("/restheart/target/restheart-1.0-SNAPSHOT-jar-with-dependencies.jar") }
 end
