449e0fab96ae0d68286b043e387bc55b8ad8a778
Andrea Di Cesare <andrea@softinstigate.com>
2014-07-27 17:11:14 +0200

    added more configuration properties most important are: custom keystore, https+http+ajp listeners and performance parameters (io-threads and worker-threads, buffer options, etc)

--- src/main/java/com/softinstigate/restheart/Bootstrapper.java
+++ src/main/java/com/softinstigate/restheart/Bootstrapper.java
@@ -62,6 +62,7 @@ import org.yaml.snakeyaml.Yaml;
 
 import static io.undertow.Handlers.resource;
 import static io.undertow.Handlers.path;
+import io.undertow.Undertow.Builder;
 import io.undertow.security.api.AuthenticationMechanism;
 import io.undertow.security.api.AuthenticationMode;
 import io.undertow.security.handlers.AuthenticationCallHandler;
@@ -70,6 +71,7 @@ import io.undertow.security.handlers.AuthenticationMechanismsHandler;
 import io.undertow.security.handlers.SecurityInitialHandler;
 import io.undertow.security.impl.BasicAuthenticationMechanism;
 import io.undertow.server.HttpHandler;
+import java.net.BindException;
 import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.List;
@@ -83,7 +85,7 @@ public class Bootstrapper
     private static Undertow server;
 
     private static final Logger logger = LoggerFactory.getLogger(Bootstrapper.class);
-    
+
     private static File browserRootFile = null;
 
     public static void main(final String[] args)
@@ -109,32 +111,63 @@ public class Bootstrapper
         }
         catch (FileNotFoundException ex)
         {
-            System.err.println("cannot find the configuration file");
+            System.err.println("cannot find the configuration file. exiting..");
             System.exit(-2);
         }
 
-        int port = (Integer) conf.getOrDefault("port", "443");
-        boolean useEmbeddedKeystore = (Boolean) conf.getOrDefault("use-embedded-keystore", "true");
+        boolean httpsListener = (Boolean) conf.getOrDefault("https-listener", true);
+        int httpsPort = (Integer) conf.getOrDefault("https-port", "8443");
+        String httpsHost = (String) conf.getOrDefault("https-host", "0.0.0.0");
+
+        boolean httpListener = (Boolean) conf.getOrDefault("http-listener", false);
+        int httpPort = (Integer) conf.getOrDefault("http-port", "8080");
+        String httpHost = (String) conf.getOrDefault("http-host", "0.0.0.0");
+
+        boolean ajpListener = (Boolean) conf.getOrDefault("ajp-listener", false);
+        int ajpPort = (Integer) conf.getOrDefault("ajp-port", "8009");
+        String ajpHost = (String) conf.getOrDefault("ajp-host", "0.0.0.0");
+
+        boolean useEmbeddedKeystore = (Boolean) conf.getOrDefault("use-embedded-keystore", true);
         String keystoreFile = (String) conf.get("keystore-file");
         String keystorePassword = (String) conf.get("keystore-password");
         String certPassword = (String) conf.get("certpassword");
+
         String mongoHost = (String) conf.getOrDefault("mongo-host", "127.0.0.1");
         int mongoPort = (Integer) conf.getOrDefault("mongo-port", 27017);
         String mongoUser = (String) conf.getOrDefault("mongo-user", "");
         String mongoPassword = (String) conf.getOrDefault("mongo-password", "");
 
+        int ioThreads = (Integer) conf.getOrDefault("io-threads", 8);
+        int workerThreads = (Integer) conf.getOrDefault("worker-threads", 500);
+        int bufferSize = (Integer) conf.getOrDefault("buffer-size", 16384);
+        int buffersPerRegion = (Integer) conf.getOrDefault("buffers-per-region", 20);
+        boolean directBuffers = (Boolean) conf.getOrDefault("direct-buffers", "true");
+
         try
         {
             MongoDBClientSingleton.init(mongoHost, mongoPort, mongoUser, mongoPassword);
-            start(port, useEmbeddedKeystore, keystoreFile, keystorePassword, certPassword);
-
         }
         catch (Throwable t)
         {
-            logger.error("exiting", t);
+            logger.error("error connecting to mongodb. exiting..", t);
             System.exit(-1);
         }
 
+        try
+        {
+            start(
+                httpsListener, httpsHost, httpsPort,
+                httpListener, httpHost, httpPort,
+                ajpListener, ajpHost, ajpPort,
+                useEmbeddedKeystore, keystoreFile, keystorePassword, certPassword,
+                ioThreads, workerThreads, bufferSize, buffersPerRegion, directBuffers);
+        }
+        catch (Throwable t)
+        {
+            logger.error("error starting restheart. exiting..", t);
+            System.exit(-2);
+        }
+        
         Runtime.getRuntime().addShutdownHook(new Thread()
         {
             @Override
@@ -162,7 +195,7 @@ public class Bootstrapper
                 {
                     logger.error("error flushing and clonsing the mongo cliet", t);
                 }
-                
+
                 if (browserRootFile != null)
                 {
                     try
@@ -178,16 +211,37 @@ public class Bootstrapper
                 logger.info("restheart stopped");
             }
         });
-
-        logger.info("restheart started on port " + port);
+        
+        logger.info("restheart started");
     }
 
-    private static void start(int port,
+    private static void start(
+            boolean httpsListener,
+            String httpsHost,
+            int httpsPort,
+            boolean httpListener,
+            String httpHost,
+            int httpPort,
+            boolean ajpListener,
+            String ajpHost,
+            int ajpPort,
             boolean useEmbeddedKeystore,
             String keystoreFile,
             String keystorePassword,
-            String certPassword)
+            String certPassword,
+            int ioThreads,
+            int workerThreads,
+            int bufferSize,
+            int buffersPerRegion,
+            boolean directBuffers
+    )
     {
+        if (!httpsListener && !httpListener && !ajpListener)
+        {
+            logger.error("no listener specified. exiting..");
+            System.exit(-1);
+        }
+
         final Map<String, char[]> users = new HashMap<>(2);
         users.put("admin", "admin".toCharArray());
         users.put("user", "user".toCharArray());
@@ -218,22 +272,31 @@ public class Bootstrapper
             }
             else
             {
-                throw new IllegalArgumentException("custom keyfactory not yet implemented");
+                sslContext = SSLContext.getInstance("TLS");
+                kmf = KeyManagerFactory.getInstance("SunX509");
+                ks = KeyStore.getInstance("JKS");
+
+                FileInputStream fis = new FileInputStream(new File(keystoreFile));
+
+                ks.load(fis, keystorePassword.toCharArray());
+
+                kmf.init(ks, certPassword.toCharArray());
+                sslContext.init(kmf.getKeyManagers(), null, null);
             }
         }
         catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException | CertificateException | UnrecoverableKeyException ex)
         {
-            logger.error("couldn't start restheart, error with specified keystore", ex);
+            logger.error("couldn't start restheart, error with specified keystore. exiting..", ex);
             System.exit(-1);
         }
         catch (FileNotFoundException ex)
         {
-            logger.error("couldn't start restheart, keystore file not found", ex);
+            logger.error("couldn't start restheart, keystore file not found. exiting..", ex);
             System.exit(-1);
         }
         catch (IOException ex)
         {
-            logger.error("couldn't start restheart, error reading the keystore file", ex);
+            logger.error("couldn't start restheart, error reading the keystore file. exiting..", ex);
             System.exit(-1);
         }
 
@@ -243,15 +306,38 @@ public class Bootstrapper
         }
         catch (URISyntaxException | IOException ex)
         {
-            logger.error("error instanitating browser web app", ex);
+            logger.error("error instanitating browser web app. exiting..", ex);
             System.exit(-1);
         }
-        
+
         logger.info("static resources are in {}", browserRootFile.toString());
 
-        server = Undertow.builder()
-                .addHttpsListener(port, "0.0.0.0", sslContext)
-                .setWorkerThreads(50)
+        Builder builder = Undertow.builder();
+
+        if (httpsListener)
+        {
+            builder.addHttpsListener(httpsPort, httpsHost, sslContext);
+            logger.info("https listener bound at {}:{}", httpsHost, httpsPort);
+        }
+
+        if (httpListener)
+        {
+            builder.addHttpListener(httpPort, httpHost);
+            logger.info("http listener bound at {}:{}", httpHost, httpPort);
+        }
+
+        if (ajpListener)
+        {
+            builder.addAjpListener(ajpPort, ajpHost);
+            logger.info("ajp listener bound at {}:{}", ajpHost, ajpPort);
+        }
+
+        builder
+                .setIoThreads(ioThreads)
+                .setWorkerThreads(workerThreads)
+                .setDirectBuffers(directBuffers)
+                .setBufferSize(bufferSize)
+                .setBuffersPerRegion(buffersPerRegion)
                 .setHandler(
                         path()
                         .addPrefixPath("/@browser", resource(new FileResourceManager(browserRootFile, 3)).addWelcomeFiles("browser.html").setDirectoryListingEnabled(false))
@@ -287,9 +373,9 @@ public class Bootstrapper
                                                         )
                                                 )
                                         ), identityManager)
-                        ))
-                .build();
-        server.start();
+                        ));
+
+        builder.build().start();
     }
 
     private static HttpHandler addSecurity(final HttpHandler toWrap, final IdentityManager identityManager)
