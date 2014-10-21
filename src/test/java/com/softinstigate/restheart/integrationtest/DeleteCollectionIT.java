85183af25c55219224f589cd4900b976112d8eb3
Andrea Di Cesare <andrea@softinstigate.com>
2014-10-21 23:20:12 +0200

    test DELETE /db test DELETE /db/coll

--- src/test/java/com/softinstigate/restheart/integrationtest/DeleteCollectionIT.java
+++ src/test/java/com/softinstigate/restheart/integrationtest/DeleteCollectionIT.java
@@ -10,12 +10,14 @@
  */
 package com.softinstigate.restheart.integrationtest;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import com.eclipsesource.json.JsonObject;
+import com.softinstigate.restheart.hal.Representation;
+import static com.softinstigate.restheart.integrationtest.AbstactIT.adminExecutor;
+import com.softinstigate.restheart.utils.HttpStatus;
+import io.undertow.util.Headers;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -28,9 +30,46 @@ public class DeleteCollectionIT extends AbstactIT
     {
     }
     
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void testDeleteDocument() throws Exception
+    {
+        try
+        {
+            Response resp;
+
+            // *** PUT tmpdb
+            resp = adminExecutor.execute(Request.Put(dbTmpUri).bodyString("{a:1}", halCT).addHeader(Headers.CONTENT_TYPE_STRING, Representation.HAL_JSON_MEDIA_TYPE));
+            check("check put db", resp, HttpStatus.SC_CREATED);
+
+            // *** PUT tmpcoll
+            resp = adminExecutor.execute(Request.Put(collectionTmpUri).bodyString("{a:1}", halCT).addHeader(Headers.CONTENT_TYPE_STRING, Representation.HAL_JSON_MEDIA_TYPE));
+            check("check put coll1", resp, HttpStatus.SC_CREATED);
+
+            // try to delete without etag
+            resp = adminExecutor.execute(Request.Delete(collectionTmpUri));
+            check("check delete tmp doc without etag", resp, HttpStatus.SC_CONFLICT);
+            
+            // try to delete with wrong etag
+            resp = adminExecutor.execute(Request.Delete(collectionTmpUri).addHeader(Headers.IF_MATCH_STRING, "pippoetag"));
+            check("check delete tmp doc with wrong etag", resp, HttpStatus.SC_PRECONDITION_FAILED);
+            
+            resp = adminExecutor.execute(Request.Get(collectionTmpUri).addHeader(Headers.CONTENT_TYPE_STRING, Representation.HAL_JSON_MEDIA_TYPE));
+            //check("getting etag of tmp doc", resp, HttpStatus.SC_OK);
+            
+            JsonObject content = JsonObject.readFrom(resp.returnContent().asString());
+            
+            String etag = content.get("_etag").asString();
+            
+            // try to delete with correct etag
+            resp = adminExecutor.execute(Request.Delete(collectionTmpUri).addHeader(Headers.IF_MATCH_STRING, etag));
+            check("check delete tmp doc with correct etag", resp, HttpStatus.SC_GONE);
+
+            resp = adminExecutor.execute(Request.Get(collectionTmpUri).addHeader(Headers.CONTENT_TYPE_STRING, Representation.HAL_JSON_MEDIA_TYPE));
+            check("check get deleted tmp doc", resp, HttpStatus.SC_NOT_FOUND);
+        }
+        finally
+        {
+            mongoClient.dropDatabase(dbTmpName);
+        }
+    }
 }
