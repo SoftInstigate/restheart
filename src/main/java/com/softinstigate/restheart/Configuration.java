bcb4dab3c583ef64d5358a57925e026e92e4cb28
Andrea Di Cesare <andrea@softinstigate.com>
2014-11-16 18:59:27 +0100

    version set to 0.9.2

--- src/main/java/com/softinstigate/restheart/Configuration.java
+++ src/main/java/com/softinstigate/restheart/Configuration.java
@@ -40,7 +40,7 @@ public class Configuration {
     /**
      * the restheart version
      */
-    public static final String RESTHEART_VERSION = "0.9.2-SNAPSHOT";
+    public static final String RESTHEART_VERSION = "0.9.2";
 
     /**
      * URL pointing to the online documentation specific for this version.
