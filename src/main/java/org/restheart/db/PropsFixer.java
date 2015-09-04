7e82547aa443f7f4376df53a466b0d55a28f3393
Maurizio Turatti <maurizio.turatti@gmail.com>
2015-09-04 19:19:13 +0200

    Replace deprecated exception with MongoCommandException

--- src/main/java/org/restheart/db/PropsFixer.java
+++ src/main/java/org/restheart/db/PropsFixer.java
@@ -18,7 +18,7 @@
 package org.restheart.db;
 
 import com.mongodb.BasicDBObject;
-import com.mongodb.CommandFailureException;
+import com.mongodb.MongoCommandException;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
@@ -154,10 +154,10 @@ public class PropsFixer {
                 }
                 );
             });
-        } catch (CommandFailureException cfe) {
-            Object errmsg = cfe.getCommandResult().get("errmsg");
+        } catch (MongoCommandException cfe) {
+            String errmsg = cfe.getErrorMessage();
 
-            if (errmsg != null && errmsg instanceof String && ("unauthorized".equals(errmsg) || ((String) errmsg).contains("not authorized"))) {
+            if (errmsg != null && ("unauthorized".equals(errmsg) || errmsg.contains("not authorized"))) {
                 LOGGER.error("error looking for dbs and collections with missing _properties due to insuffient mongo user privileges. note that requests to dbs and collections with no _properties result on NOT_FOUND", cfe);
             } else {
                 LOGGER.error("eorro looking for dbs and collections with missing _properties. note that requests to dbs and collections with no _properties result on NOT_FOUND", cfe);
