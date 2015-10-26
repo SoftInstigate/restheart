2531ec7c8319063fbcb0e5ac5d6c942e8c1a27f3
Maurizio Turatti <maurizio.turatti@gmail.com>
2015-10-26 20:44:07 +0100

    Rename CLIENT_HOST to MONGO_HOST and get it from environment variables
    modified:   src/test/java/org/restheart/handlers/files/GetFileHandlerIT.java
    modified:   src/test/java/org/restheart/test/integration/AbstactIT.java
    modified:   src/test/java/org/restheart/test/integration/PutIndexIT.java
    modified:   src/test/java/org/restheart/test/integration/SecurityAuthTokenIT.java

--- src/test/java/org/restheart/test/integration/SecurityAuthTokenIT.java
+++ src/test/java/org/restheart/test/integration/SecurityAuthTokenIT.java
@@ -125,7 +125,7 @@ public class SecurityAuthTokenIT extends AbstactIT {
 
         URI authTokenResourceUri = rootUri.resolve(locationURI);
         
-        final String host = CLIENT_HOST;
+        final String host = MONGO_HOST;
         final int port = conf.getHttpPort();
 
         Response resp2 = unauthExecutor.authPreemptive(new HttpHost(host, port, HTTP)).auth(new Credentials() {
