7f0dbdaa121f9790092ca584c9c9e55e48668960
Andrea Di Cesare <andrea@softinstigate.com>
2016-03-18 16:55:43 +0100

    added first integration test for json schema validation

--- src/test/java/org/restheart/test/integration/JsonSchemaCheckerIT.java
+++ src/test/java/org/restheart/test/integration/JsonSchemaCheckerIT.java
@@ -23,6 +23,7 @@ import java.net.URISyntaxException;
 import org.apache.http.HttpStatus;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -30,63 +31,156 @@ import org.junit.Test;
  */
 public class JsonSchemaCheckerIT extends AbstactIT {
     private final String DB = "test-jsonschema-db";
-    private final String COLL = "coll";
+    private final String COLL_BASIC = "coll_basic";
+    private final String COLL_CHILD = "coll_child";
     private final String SCHEMA_STORE = "_schemas";
-    
+
     public JsonSchemaCheckerIT() throws URISyntaxException {
     }
 
     @Before
     public void createTestData() throws Exception {
         HttpResponse resp;
-        
+
         // create test db
         resp = Unirest.put(url(DB))
                 .basicAuth(ADMIN_ID, ADMIN_PWD)
-                .asJson() ;
+                .asString();
 
         Assert.assertEquals("create db " + DB, HttpStatus.SC_CREATED, resp.getStatus());
-        
-        // create test collection
-        resp = Unirest.put(url(DB, COLL))
-                .basicAuth(ADMIN_ID, ADMIN_PWD)
-                .asJson() ;
 
-        Assert.assertEquals("create collection " + DB.concat("/").concat(COLL), HttpStatus.SC_CREATED, resp.getStatus());
-        
         // create schema store
         resp = Unirest.put(url(DB, SCHEMA_STORE))
                 .basicAuth(ADMIN_ID, ADMIN_PWD)
-                .asJson() ;
+                .asString();
 
         Assert.assertEquals("create schema store " + DB.concat("/").concat(SCHEMA_STORE), HttpStatus.SC_CREATED, resp.getStatus());
+
+        // create schemas
+        resp = Unirest.put(url(DB, SCHEMA_STORE, "basic"))
+                .basicAuth(ADMIN_ID, ADMIN_PWD)
+                .header("content-type", "application/json")
+                .body(getResourceFile("schemas/basic.json"))
+                .asString();
+
+        Assert.assertEquals("create schema basic.json", HttpStatus.SC_CREATED, resp.getStatus());
+
+        resp = Unirest.put(url(DB, SCHEMA_STORE, "parent"))
+                .basicAuth(ADMIN_ID, ADMIN_PWD)
+                .header("content-type", "application/json")
+                .body(getResourceFile("schemas/parent.json"))
+                .asString();
+
+        Assert.assertEquals("create schema parent.json", HttpStatus.SC_CREATED, resp.getStatus());
+
+        resp = Unirest.put(url(DB, SCHEMA_STORE, "child"))
+                .basicAuth(ADMIN_ID, ADMIN_PWD)
+                .header("content-type", "application/json")
+                .body(getResourceFile("schemas/child.json"))
+                .asString();
+
+        Assert.assertEquals("create schema child.json", HttpStatus.SC_CREATED, resp.getStatus());
+
+        // create test collection basic
+        resp = Unirest.put(url(DB, COLL_BASIC))
+                .basicAuth(ADMIN_ID, ADMIN_PWD)
+                .header("content-type", "application/json")
+                .body("{'checkers': [ { 'name': 'jsonSchema', 'args': { 'schemaId': 'basic' }, 'skipNotSupported': true } ] }")
+                .asString();
+
+        Assert.assertEquals("create collection " + DB.concat("/").concat(COLL_BASIC), HttpStatus.SC_CREATED, resp.getStatus());
+
+        // create test child
+        resp = Unirest.put(url(DB, COLL_CHILD))
+                .basicAuth(ADMIN_ID, ADMIN_PWD)
+                .header("content-type", "application/json")
+                .body("{'checkers': [ { 'name': 'jsonSchema', 'args': { 'schemaId': 'child' }, 'skipNotSupported': true } ] }")
+                .asString();
+
+        Assert.assertEquals("create collection " + DB.concat("/").concat(COLL_CHILD), HttpStatus.SC_CREATED, resp.getStatus());
     }
 
     @Test
     public void testPostData() throws Exception {
+        HttpResponse resp;
+        
         // *** test create invalid data
+        
+        resp = Unirest.post(url(DB, COLL_BASIC))
+                .basicAuth(ADMIN_ID, ADMIN_PWD)
+                .header("content-type", "application/json")
+                .body("{'n': 'ciao', 's': 'a' }")
+                .asString();
+        
+        Assert.assertEquals("test invalid data 1", HttpStatus.SC_BAD_REQUEST, resp.getStatus());
+        
+        resp = Unirest.post(url(DB, COLL_BASIC))
+                .basicAuth(ADMIN_ID, ADMIN_PWD)
+                .header("content-type", "application/json")
+                .body("{'n': 1, 's': 1 }")
+                .asString();
+        
+        Assert.assertEquals("test invalid data 2", HttpStatus.SC_BAD_REQUEST, resp.getStatus());
+        
+        resp = Unirest.post(url(DB, COLL_BASIC))
+                .basicAuth(ADMIN_ID, ADMIN_PWD)
+                .header("content-type", "application/json")
+                .body("{'s': 'string' }")
+                .asString();
+        
+        Assert.assertEquals("test invalid data 3", HttpStatus.SC_BAD_REQUEST, resp.getStatus());
 
         // *** test create valid data
-
+        
+        resp = Unirest.put(url(DB, COLL_BASIC, "doc"))
+                .basicAuth(ADMIN_ID, ADMIN_PWD)
+                .header("content-type", "application/json")
+                .body("{'n': 1, 's': 'string' }")
+                .asString();
+        
+        Assert.assertEquals("test create valid data", HttpStatus.SC_CREATED, resp.getStatus());
+        
         // *** test update invalid data
+        
+        resp = Unirest.patch(url(DB, COLL_BASIC, "doc"))
+                .basicAuth(ADMIN_ID, ADMIN_PWD)
+                .header("content-type", "application/json")
+                .body("{'n': 'string' }")
+                .asString();
+        
+        Assert.assertEquals("test update invalid data", HttpStatus.SC_BAD_REQUEST, resp.getStatus());
+        
+        // *** test update valid data
+        
+        resp = Unirest.patch(url(DB, COLL_BASIC, "doc"))
+                .basicAuth(ADMIN_ID, ADMIN_PWD)
+                .header("content-type", "application/json")
+                .body("{'n': 100 }")
+                .asString();
+        
+        Assert.assertEquals("test update valid data", HttpStatus.SC_BAD_REQUEST, resp.getStatus());
     }
 
     @Test
+    @Ignore
     public void testPostDataDotNotation() throws Exception {
         // *** test post valid data with dot notation
     }
 
     @Test
+    @Ignore
     public void testPostIncompleteDataDotNotation() throws Exception {
         // *** test post valid data with dot notation
     }
 
     @Test
+    @Ignore
     public void testPutDataDotNotation() throws Exception {
         // *** test post valid data with dot notation
     }
 
     @Test
+    @Ignore
     public void testPatchData() throws Exception {
         // *** test create valid data
         // *** test patch valid data with dot notation
@@ -102,6 +196,7 @@ public class JsonSchemaCheckerIT extends AbstactIT {
      * @throws Exception
      */
     @Test
+    @Ignore
     public void testPatchIncompleteObject() throws Exception {
         // *** test create valid data
 
