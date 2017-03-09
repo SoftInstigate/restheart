d6eb8dac8c6155dce1b20947f1821b3188680094
Maurizio Turatti <maurizio.turatti@gmail.com>
2017-03-09 22:36:38 +0100

    Automatic build of docker image
    modified:   .travis.yml
    modified:   Docker/build.sh
    modified:   Docker/docker-compose.yml
    modified:   Docker/etc/security.yml
    new file:   project-version.sh

---
+++ project-version.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export MVN_VERSION=$(mvn -q \
+    -Dexec.executable="echo" \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec 2>/dev/null)
+echo $MVN_VERSION
