8fc371033886d9f9e2df27d43ec1811c029a1748
Maurizio Turatti <maurizio.turatti@gmail.com>
2017-03-09 19:10:07 +0100

    modified:   project-version.sh

--- project-version.sh
+++ project-version.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-mvn help:evaluate -Dexpression=project.version | grep -e '^[^\[]'
+export MVN_VERSION=$(mvn -q \
+    -Dexec.executable="echo" \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+echo $MVN_VERSION
