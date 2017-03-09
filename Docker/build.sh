c3cc09936961fcf181067f09c2f7c9bd250ed0d3
Maurizio Turatti <maurizio.turatti@gmail.com>
2017-03-09 18:14:35 +0100

    Refactoring of docker-compose.yml
    modified:   build.sh
    modified:   docker-compose.yml

--- Docker/build.sh
+++ Docker/build.sh
@@ -2,7 +2,10 @@
 cd ..
 git submodule update --init --recursive
 mvn clean package -DskipTests=true
+export RHVERSION=$(./project-version.sh)
+echo "Building Docker image for RESTHeart Version "$RHVERSION
 cd Docker
 cp ../target/restheart.jar .
-docker build -t restheart-snapshot .
+docker build -t restheart .
+docker tag restheart restheart:$RHVERSION
 rm restheart.jar
