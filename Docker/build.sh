509424e5c73034d2f020758faf2cd32298c49604
Maurizio Turatti <maurizio.turatti@gmail.com>
2017-03-09 16:57:01 +0100

    modified:   Docker/build.sh

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
 git submodule update --init --recursive
-mvn package -DskipTests=true
+mvn clean package -DskipTests=true
 cd Docker
 cp ../target/restheart.jar .
 docker build -t restheart-snapshot .
