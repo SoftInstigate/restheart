cad5f790e68a2a8ead193a24772aad3b830ec6b7
Maurizio Turatti <maurizio.turatti@gmail.com>
2017-03-09 19:28:47 +0100

    modified:   project-version.sh

--- project-version.sh
+++ project-version.sh
@@ -3,5 +3,5 @@ export MVN_VERSION=$(mvn -q \
     -Dexec.executable="echo" \
     -Dexec.args='${project.version}' \
     --non-recursive \
-    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec 2>/dev/null)
 echo $MVN_VERSION
