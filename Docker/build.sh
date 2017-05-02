59634f0b75f81399386aa0a345ea8aa9701fec62
Maurizio Turatti <maurizio.turatti@gmail.com>
2017-05-02 16:06:18 +0200

    Refactoring of build.sh and project-version.sh
    modified:   Docker/build.sh
    deleted:    project-version.sh

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-cd ..
+set -e
+
+echo "###### start build.sh #####"
+
+PRESENT_DIR="$PWD";
+cd "$(dirname ${BASH_SOURCE[0]})"/..
+
 git submodule update --init --recursive
 mvn clean package -DskipTests=true
-export RHVERSION=$(./project-version.sh)
-echo "Building Docker image for RESTHeart Version "$RHVERSION
+export RHVERSION=$(./bin/project-version.sh)
+echo "###### Building Docker image for RESTHeart Version "$RHVERSION
 cd Docker
 cp ../target/restheart.jar .
 docker build -t restheart .
 docker tag restheart restheart:$RHVERSION
 rm restheart.jar
+cd "$PRESENT_DIR"
+echo "###### end build.sh #####"
