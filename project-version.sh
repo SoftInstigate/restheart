a4c53889a3e7b6e81acafae22f499c6c85897332
Maurizio Turatti <maurizio.turatti@gmail.com>
2017-05-02 16:06:18 +0200

    Refactoring of build.sh and project-version.sh
    modified:   Docker/build.sh
    deleted:    project-version.sh

--- project-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export MVN_VERSION=$(mvn --quiet \
-    -Dexec.executable="echo" \
-    -Dexec.args='${project.version}' \
-    --non-recursive \
-    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec 2>/dev/null)
-echo $MVN_VERSION
