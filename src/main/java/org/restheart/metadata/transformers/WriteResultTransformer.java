8c008d46f507701c5df89925ad5de91b8f61ed0f
Maurizio Turatti <maurizio.turatti@gmail.com>
2017-07-02 10:49:03 +0200

    Refactoring

--- src/main/java/org/restheart/metadata/transformers/WriteResultTransformer.java
+++ src/main/java/org/restheart/metadata/transformers/WriteResultTransformer.java
@@ -1,23 +1,5 @@
-/*
- * RESTHeart - the Web API for MongoDB
- * Copyright (C) SoftInstigate Srl
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.restheart.metadata.transformers;
 
-import org.restheart.metadata.transformers.Transformer;
 import io.undertow.server.HttpServerExchange;
 import org.bson.BsonDocument;
 import org.bson.BsonNull;
@@ -29,6 +11,7 @@ import org.restheart.handlers.RequestContext;
  * @author Andrea Di Cesare <andrea@softinstigate.com>
  */
 public class WriteResultTransformer implements Transformer {
+
     @Override
     public void transform(
             HttpServerExchange exchange,
@@ -36,7 +19,6 @@ public class WriteResultTransformer implements Transformer {
             BsonValue contentToTransform,
             BsonValue args) {
         if (context.getDbOperationResult() == null) {
-            return;
         } else {
             BsonDocument resp = null;
 
@@ -52,7 +34,7 @@ public class WriteResultTransformer implements Transformer {
                         == null
                                 ? new BsonNull()
                                 : context.getDbOperationResult().getOldData());
-                
+
                 resp.append("newData", context.getDbOperationResult().getNewData()
                         == null
                                 ? new BsonNull()
