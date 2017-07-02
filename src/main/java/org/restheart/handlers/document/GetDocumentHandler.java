51a7c65cff519039efb815e1ebdab056ca921a34
Maurizio Turatti <maurizio.turatti@gmail.com>
2017-07-02 10:29:11 +0200

    Refactoring

--- src/main/java/org/restheart/handlers/document/GetDocumentHandler.java
+++ src/main/java/org/restheart/handlers/document/GetDocumentHandler.java
@@ -21,19 +21,19 @@ import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
 import io.undertow.server.HttpServerExchange;
 import java.util.Deque;
+import java.util.HashSet;
 import org.bson.BsonDocument;
 import org.bson.BsonObjectId;
 import org.bson.conversions.Bson;
 import org.restheart.hal.Representation;
 import org.restheart.handlers.PipedHttpHandler;
 import org.restheart.handlers.RequestContext;
+import org.restheart.handlers.RequestContext.TYPE;
 import org.restheart.utils.HttpStatus;
+import org.restheart.utils.JsonUtils;
 import org.restheart.utils.RequestHelper;
 import org.restheart.utils.ResponseHelper;
 import org.restheart.utils.URLUtils;
-import org.restheart.utils.JsonUtils;
-import org.restheart.handlers.RequestContext.TYPE;
-import java.util.HashSet;
 
 /**
  *
@@ -72,15 +72,15 @@ public class GetDocumentHandler extends PipedHttpHandler {
             next(exchange, context);
             return;
         }
-        
+
         Bson query = eq("_id", context.getDocumentId());
 
         HashSet<Bson> terms = new HashSet<>();
-        
+
         if (context.getShardKey() != null) {
             terms.add(context.getShardKey());
         }
-        
+
         // filters are applied to GET /db/coll/docid as well 
         // to make easy implementing filter based access restrictions
         // for instance a Trasnformer can add a filter to limit access to data
@@ -88,7 +88,7 @@ public class GetDocumentHandler extends PipedHttpHandler {
         if (context.getFiltersDocument() != null) {
             terms.add(context.getFiltersDocument());
         }
-        
+
         if (terms.size() > 0) {
             terms.add(query);
             query = and(terms);
@@ -118,7 +118,7 @@ public class GetDocumentHandler extends PipedHttpHandler {
                     ? " does not exist"
                     : " ".concat(JsonUtils.getIdAsString(
                             context.getDocumentId(), true))
-                    .concat(" does not exist");
+                            .concat(" does not exist");
 
             if (null != context.getType()) {
                 switch (context.getType()) {
