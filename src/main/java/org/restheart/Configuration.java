ab2c3eb04dc32b6b1e56b95bb830f62248eb88dd
Nath <nath@thirststudios.co.uk>
2017-09-21 10:39:19 +0100

    RH-64 - Allow PATCHing of binary file metadata

--- src/main/java/org/restheart/Configuration.java
+++ src/main/java/org/restheart/Configuration.java
@@ -531,9 +531,9 @@ public class Configuration {
      * @param integers
      * @return
      */
-    public static int[] convertListToIntArray(List integers) {
+    public static int[] convertListToIntArray(List<Object> integers) {
         int[] ret = new int[integers.size()];
-        Iterator iterator = integers.iterator();
+        Iterator<Object> iterator = integers.iterator();
         for (int i = 0; i < ret.length; i++) {
             Object o = iterator.next();
 
@@ -1002,7 +1002,6 @@ public class Configuration {
      * @param defaultValue
      * @return
      */
-    @SuppressWarnings("unchecked")
     private Boolean getAsBooleanOrDefault(final Map<String, Object> conf, final String key, final Boolean defaultValue) {
         if (conf == null) {
             if (!silent) {
@@ -1039,7 +1038,6 @@ public class Configuration {
      * @param defaultValue
      * @return
      */
-    @SuppressWarnings("unchecked")
     private String getAsStringOrDefault(final Map<String, Object> conf, final String key, final String defaultValue) {
 
         if (conf == null || conf.get(key) == null) {
@@ -1068,7 +1066,6 @@ public class Configuration {
      * @param defaultValue
      * @return
      */
-    @SuppressWarnings("unchecked")
     private Integer getAsIntegerOrDefault(final Map<String, Object> conf, final String key, final Integer defaultValue) {
         if (conf == null || conf.get(key) == null) {
             // if default value is null there is no default value actually
@@ -1096,7 +1093,6 @@ public class Configuration {
      * @param defaultValue
      * @return
      */
-    @SuppressWarnings("unchecked")
     private Long getAsLongOrDefault(final Map<String, Object> conf, final String key, final Long defaultValue) {
         if (conf == null || conf.get(key) == null) {
             // if default value is null there is no default value actually
@@ -1144,7 +1140,7 @@ public class Configuration {
                 LOGGER.debug("paramenter {} set to {}", key, conf.get(key));
             }
 
-            int ret[] = convertListToIntArray((List) conf.get(key));
+            int ret[] = convertListToIntArray((List<Object>) conf.get(key));
 
             if (ret.length == 0) {
                 if (!silent) {
