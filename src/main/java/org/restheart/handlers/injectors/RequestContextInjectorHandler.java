75876d3c6d3fe5f318a830abff0f3382dba82712
Lena Brueder <brueder@espirit.com>
2017-10-23 13:42:28 +0200

    added tests, changes some production code to make it easier to test

--- src/main/java/org/restheart/handlers/injectors/RequestContextInjectorHandler.java
+++ src/main/java/org/restheart/handlers/injectors/RequestContextInjectorHandler.java
@@ -119,7 +119,7 @@ public class RequestContextInjectorHandler extends PipedHttpHandler {
                 try {
                     rep = REPRESENTATION_FORMAT.valueOf(_rep.trim().toUpperCase());
                 } catch (IllegalArgumentException iae) {
-                    rcontext.addWarning("illegal rep paramenter " + _rep + " (must be PLAIN_JSON, PJ or HAL)");
+                    rcontext.addWarning("illegal rep parameter " + _rep + " (must be PLAIN_JSON, PJ or HAL)");
                 }
             }
         }
