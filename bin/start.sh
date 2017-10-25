7126febd75e319b3258eeaa5e9c3d699129e0383
Maurizio Turatti <maurizio.turatti@gmail.com>
2017-10-25 11:58:21 +0000

    Creating site for 3.2.0-SNAPSHOT

--- bin/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-java -Dfile.encoding=UTF-8 -server -jar $DIR/../target/restheart.jar $1 --fork
-echo 'Sleeping few seconds...'
-sleep 2
