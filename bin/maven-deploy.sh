eb957f683dac6f6d4030f6fee6ebe6e877142886
Maurizio Turatti <maurizio.turatti@gmail.com>
2017-12-07 18:40:16 +0100

    Improve log messages in scripts

--- bin/maven-deploy.sh
+++ bin/maven-deploy.sh
@@ -5,21 +5,21 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     RESTHEART_VERSION=$(./bin/project-version.sh 2>/dev/null);
     export RESTHEART_VERSION
     if [[ "$RESTHEART_VERSION" ]]; then
-        echo "Building Docker image for RESTHeart $RESTHEART_VERSION";
+        echo "###### Building Docker image for RESTHeart $RESTHEART_VERSION";
         docker login -u="$DOCKER_USER" -p="$DOCKER_PASS";
         cd Docker && cp ../target/restheart.jar . ;
         docker build -t softinstigate/restheart:$RESTHEART_VERSION . ;
         docker push softinstigate/restheart:$RESTHEART_VERSION;
-        echo "Branch is $TRAVIS_BRANCH";
+        echo "###### Branch is $TRAVIS_BRANCH";
         if [ "$TRAVIS_BRANCH" == "master" ]; then
-            echo "On master branch. Tagging image softinstigate/restheart:$RESTHEART_VERSION as latest";
+            echo "###### On master branch. Tagging image softinstigate/restheart:$RESTHEART_VERSION as latest";
             docker tag softinstigate/restheart:$RESTHEART_VERSION softinstigate/restheart:latest;
             docker push softinstigate/restheart:latest;
-            echo "Publishing Maven site at http://softinstigate.github.io/restheart/project-info.html";
+            echo "###### Publishing Maven site at http://softinstigate.github.io/restheart/project-info.html";
             cd .. && mvn site --settings deploy-settings.xml -P report -Dmaven.test.skip=true;
         fi
     else
-        echo "ERROR! Variable RESTHEART_VERSION is undefined";
+        echo "###### ERROR! Variable RESTHEART_VERSION is undefined";
         exit 1;
     fi
 fi
