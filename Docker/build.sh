ee76d1f4928dd4562ef5e2340d55ce37558edb50
Maurizio Turatti <maurizio.turatti@gmail.com>
2018-03-14 10:03:32 +0100

    Rename local image to softinstigate/restheart

--- Docker/build.sh
+++ Docker/build.sh
@@ -12,8 +12,8 @@ export RHVERSION=$(./bin/project-version.sh)
 echo "###### Building Docker image for RESTHeart Version "$RHVERSION
 cd Docker
 cp ../target/restheart.jar .
-docker build -t restheart .
-docker tag restheart restheart:$RHVERSION
+docker build -t softinstigate/restheart .
+docker tag softinstigate/restheart softinstigate/restheart:$RHVERSION
 rm restheart.jar
 cd "$PRESENT_DIR"
 echo "###### end build.sh #####"
