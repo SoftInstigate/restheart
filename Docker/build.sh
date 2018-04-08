6fe8906295b0785594948575cd2ecd4b68b8f5d6
Maurizio Turatti <maurizio.turatti@gmail.com>
2018-04-08 12:16:58 +0200

    Modify Docker/build.sh to work in the root folder

--- Docker/build.sh
+++ Docker/build.sh
@@ -3,17 +3,12 @@ set -e
 
 echo "###### start build.sh #####"
 
-PRESENT_DIR="$PWD";
 cd "$(dirname ${BASH_SOURCE[0]})"/..
 
 git submodule update --init --recursive
-mvn clean package -DskipTests=true
+mvn clean package
 export RHVERSION=$(./bin/project-version.sh)
 echo "###### Building Docker image for RESTHeart Version "$RHVERSION
-cd Docker
-cp ../target/restheart.jar .
 docker build -t softinstigate/restheart .
 docker tag softinstigate/restheart softinstigate/restheart:$RHVERSION
-rm restheart.jar
-cd "$PRESENT_DIR"
 echo "###### end build.sh #####"
