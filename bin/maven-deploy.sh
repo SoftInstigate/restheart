b1838be1ff520b432a6cd451ab3bd368ab811852
Maurizio Turatti <maurizio.turatti@gmail.com>
2018-07-07 12:05:29 +0200

    Don't push the Docker tag when it's a SNAPSHOT

--- bin/maven-deploy.sh
+++ bin/maven-deploy.sh
@@ -7,12 +7,14 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [[ "$RESTHEART_VERSION" ]]; then
         echo "###### Building Docker image for RESTHeart $RESTHEART_VERSION";
         docker login -u="$DOCKER_USER" -p="$DOCKER_PASS";
-        docker build -t softinstigate/restheart:$RESTHEART_VERSION . ;
-        docker push softinstigate/restheart:$RESTHEART_VERSION;
+        docker build -t "softinstigate/restheart:$RESTHEART_VERSION" . ;
+        if [[ $RESTHEART_VERSION != *-SNAPSHOT ]]; then
+            docker push "softinstigate/restheart:$RESTHEART_VERSION";
+        fi
         echo "###### Branch is $TRAVIS_BRANCH";
         if [ "$TRAVIS_BRANCH" == "master" ]; then
             echo "###### On master branch. Tagging image softinstigate/restheart:$RESTHEART_VERSION as latest";
-            docker tag softinstigate/restheart:$RESTHEART_VERSION softinstigate/restheart:latest;
+            docker tag "softinstigate/restheart:$RESTHEART_VERSION softinstigate/restheart:latest";
             docker push softinstigate/restheart:latest;
             echo "###### Publishing Maven site at http://softinstigate.github.io/restheart/project-info.html";
             mvn site --settings deploy-settings.xml -P report -Dmaven.test.skip=true;
