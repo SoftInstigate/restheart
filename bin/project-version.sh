93105effcc7a0ac2aff806beae74a40d2048681b
Maurizio Turatti <maurizio.turatti@gmail.com>
2018-10-19 00:45:32 +0200

    Add "set -e" to build shell scripts

--- bin/project-version.sh
+++ bin/project-version.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -e
+
 PRESENT_DIR="$PWD";
-cd "$(dirname ${BASH_SOURCE[0]})"/..
-export MVN_VERSION=$(mvn --quiet \
+cd "$(dirname ${BASH_SOURCE[0]})"/.. || exit 1
+MVN_VERSION=$(mvn --quiet \
     -Dexec.executable="echo" \
     -Dexec.args='${project.version}' \
     --non-recursive \
     org.codehaus.mojo:exec-maven-plugin:1.3.1:exec 2>/dev/null)
-echo $MVN_VERSION
-cd "$PRESENT_DIR"
+export MVN_VERSION
+echo "$MVN_VERSION"
+cd "$PRESENT_DIR" || exit 2
