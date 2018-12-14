f4dadeeafd5086dade14deb0c0e97cdc6b0aa5d4
Maurizio Turatti <maurizio@softinstigate.com>
2018-12-14 13:19:41 +0100

    [skip ci] Revert "Add Fossa reports (https://app.fossa.io/)"
    This reverts commit 363ad7bf2a01c331859c8563ff44d3e71f950915.

--- bin/fossa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install.sh | bash
-fossa init FOSSA_API_KEY=2fad7584cff11f3b49381e650be03be4 fossa
