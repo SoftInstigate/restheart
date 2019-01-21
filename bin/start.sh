536b472f629fb510aaa99999002ac263e1950ecb
Maurizio Turatti <maurizio@softinstigate.com>
2019-01-21 17:51:40 +0100

    Pass --fork as a parameter to start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -3,6 +3,6 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-java -Dfile.encoding=UTF-8 -server -jar $DIR/../target/restheart.jar $1 --fork
+java -Dfile.encoding=UTF-8 -server -jar "$DIR/../target/restheart.jar" "$@"
 echo 'Sleeping few seconds...'
 sleep 2
