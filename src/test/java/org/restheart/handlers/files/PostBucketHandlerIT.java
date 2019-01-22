eeb1c1cee7cc655632b85bc6041145bb483b981f
Maurizio Turatti <maurizio@softinstigate.com>
2019-01-22 21:17:26 +0100

    Add license headers on java files

--- src/test/java/org/restheart/handlers/files/PostBucketHandlerIT.java
+++ src/test/java/org/restheart/handlers/files/PostBucketHandlerIT.java
@@ -1,4 +1,20 @@
-
+/*
+ * RESTHeart - the Web API for MongoDB
+ * Copyright (C) SoftInstigate Srl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.restheart.handlers.files;
 
 import io.undertow.util.Headers;
@@ -11,7 +27,7 @@ import org.restheart.utils.HttpStatus;
 
 /**
  *
- * @author mturatti
+ * @author Maurizio Turatti {@literal <maurizio@softinstigate.com>}
  */
 public class PostBucketHandlerIT extends FileHandlerAbstractIT {
 
