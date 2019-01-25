1ca193673a02ddfff5fc015acfa4d7f3a6d1375a
Maurizio Turatti <maurizio@softinstigate.com>
2019-01-25 17:34:23 +0100

    Remove useless object

--- src/main/java/org/restheart/db/IndexDAO.java
+++ src/main/java/org/restheart/db/IndexDAO.java
@@ -1,17 +1,17 @@
 /*
  * RESTHeart - the Web API for MongoDB
  * Copyright (C) SoftInstigate Srl
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -98,11 +98,6 @@ class IndexDAO {
                     .getCollection(collection)
                     .createIndex(keys);
         } else {
-            // need to find a way to get IndexOptions from json
-            IndexOptions io = new IndexOptions();
-
-            io.background(true);
-
             client
                     .getDatabase(dbName)
                     .getCollection(collection)
