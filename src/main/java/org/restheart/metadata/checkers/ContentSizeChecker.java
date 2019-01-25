269f1f91dcefd7d8dbd8056d4f0c0a2b980ceb08
Maurizio Turatti <maurizio@softinstigate.com>
2019-01-25 18:16:28 +0100

    Fix impossible cast from org.bson.BsonValue to Integer

--- src/main/java/org/restheart/metadata/checkers/ContentSizeChecker.java
+++ src/main/java/org/restheart/metadata/checkers/ContentSizeChecker.java
@@ -1,17 +1,17 @@
 /*
  * RESTHeart - the Web API for MongoDB
  * Copyright (C) SoftInstigate Srl
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -62,10 +62,10 @@ public class ContentSizeChecker implements Checker {
             }
 
             Integer minSize = null;
-            Object _minSize = condition.get("min");
+            BsonValue _minSize = condition.get("min");
 
             if (_minSize != null) {
-                minSize = (Integer) _minSize;
+                minSize = _minSize.asNumber().intValue();
             }
 
             return (minSize == null
