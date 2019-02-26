b591b3bc6960c4849a218749a35b5a2960b1e0aa
Maurizio Turatti <maurizio@softinstigate.com>
2019-02-26 17:06:41 +0100

    mvn deploy --settings settings.xml

--- security/bin/push.sh
+++ security/bin/push.sh
@@ -2,20 +2,20 @@
 set -e
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    #mvn deploy --settings deploy-settings.xml -P release -Dmaven.test.skip=true;
+    mvn deploy --settings settings.xml -Dmaven.test.skip=true;
     VERSION=$(./bin/project-version.sh 2>/dev/null);
     export VERSION
     if [[ "$VERSION" ]]; then
         echo "###### Building Docker image for uIAM $VERSION";
         echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-        docker build -t softinstigate/uiam:$VERSION . ;
+        docker build -t "softinstigate/uiam:$VERSION" . ;
         if [[ $VERSION != *-SNAPSHOT ]]; then
-            docker push softinstigate/uiam:$VERSION;
+            docker push "softinstigate/uiam:$VERSION";
         fi
         echo "###### Branch is $TRAVIS_BRANCH";
         if [ "$TRAVIS_BRANCH" == "master" ]; then
             echo "###### On master branch. Tagging image softinstigate/uiam:$VERSION as latest";
-            docker tag softinstigate/uiam:$VERSION softinstigate/uiam:latest;
+            docker tag "softinstigate/uiam:$VERSION" softinstigate/uiam:latest;
             docker push softinstigate/uiam:latest;
             #echo "###### Publishing Maven site at http://softinstigate.github.io/uiam/project-info.html";
             #mvn site --settings deploy-settings.xml -P report -Dmaven.test.skip=true;
