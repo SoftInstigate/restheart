c6ec9fa2c52376ea3b2cc470b9553df7ed2f1cdb
Andrea Di Cesare <andrea@softinstigate.com>
2019-02-26 17:48:58 +0100

    removed useless option from start scripts

--- security/bin/start.sh
+++ security/bin/start.sh
@@ -2,6 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-java --illegal-access=deny -Dfile.encoding=UTF-8 -server -jar $DIR/../target/uiam.jar $1 --fork
+java -Dfile.encoding=UTF-8 -server -jar $DIR/../target/uiam.jar $1 --fork
 echo 'Sleeping few seconds...'
 sleep 2
