4ab54056ae287a00c36c262b954afe5244c26db5
Christian Groth <chr.groth@gmx.de>
2019-03-19 09:43:54 +0100

    provide all metrics per database and collection for prometheus using lables on root metrics endpoint only (#340)

--- src/test/java/org/restheart/handlers/applicationlogic/MetricsHandlerTest.java
+++ src/test/java/org/restheart/handlers/applicationlogic/MetricsHandlerTest.java
@@ -1,46 +1,96 @@
 package org.restheart.handlers.applicationlogic;
 
-import com.codahale.metrics.MetricRegistry;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Methods;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.restheart.Configuration.METRICS_GATHERING_LEVEL.COLLECTION;
+import static org.restheart.Configuration.METRICS_GATHERING_LEVEL.DATABASE;
+import static org.restheart.Configuration.METRICS_GATHERING_LEVEL.OFF;
+import static org.restheart.Configuration.METRICS_GATHERING_LEVEL.ROOT;
+
 import org.restheart.Configuration;
 import org.restheart.handlers.RequestContext;
 import org.restheart.utils.SharedMetricRegistryProxy;
 
 public class MetricsHandlerTest {
 
+    private static final String URI_METRICS_ROOT = "/_metrics";
+    private static final String URI_METRICS_DATABASE = "/foo/_metrics";
+    private static final String URI_METRICS_COLLECTION = "/foo/bar/_metrics";
 
     MetricsHandler handler;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         handler = new MetricsHandler(null, null);
-        handler.configuration = new Configuration();
-        handler.metrics = new SharedMetricRegistryProxy(){
-            @Override
-            public MetricRegistry registry() {
-                return new MyMetricRegistry("ROOT");
-            }
+        handler.metrics = mock(SharedMetricRegistryProxy.class);
+    }
 
-            @Override
-            public MetricRegistry registry(String dbName) {
-                return new MyMetricRegistry(dbName);
-            }
+    @Test
+    public void testRequestContextForMetricsRequestToRoot() {
+        assertRequestContextForMetricsRequest(createRequestContext(URI_METRICS_ROOT), "_metrics", null);
+    }
 
-            @Override
-            public MetricRegistry registry(String dbName, String collectionName) {
-                return new MyMetricRegistry(dbName + "/" + collectionName);
-            }
-        };
+    @Test
+    public void testRequestContextForMetricsRequestToDatabase() {
+        assertRequestContextForMetricsRequest(createRequestContext(URI_METRICS_DATABASE), "foo", "_metrics");
+    }
 
+    @Test
+    public void testRequestContextForMetricsRequestToCollection() {
+        assertRequestContextForMetricsRequest(createRequestContext(URI_METRICS_COLLECTION), "foo", "bar");
+    }
+
+    private void assertRequestContextForMetricsRequest(RequestContext requestContext, String expectedDatabaseName, String expectedCollectionName) {
+        assertEquals(expectedDatabaseName, requestContext.getDBName());
+        assertEquals(expectedCollectionName, requestContext.getCollectionName());
+    }
+
+    @Test
+    public void testMetricsLevelForRequestToRoot() {
+        assertMetricsLevelForRequest(URI_METRICS_ROOT, OFF, ROOT, ROOT, ROOT);
+    }
+
+    @Test
+    public void testMetricsLevelForRequestToDatabase() {
+        assertMetricsLevelForRequest(URI_METRICS_DATABASE, OFF, OFF, DATABASE, DATABASE);
+    }
+
+    @Test
+    public void testMetricsLevelForRequestToCollection() {
+        assertMetricsLevelForRequest(URI_METRICS_COLLECTION, OFF, OFF, OFF, COLLECTION);
+    }
+
+    private void assertMetricsLevelForRequest(String requestUri, Configuration.METRICS_GATHERING_LEVEL expectedLevelForConfigOff,
+            Configuration.METRICS_GATHERING_LEVEL expectedLevelForConfigRoot, Configuration.METRICS_GATHERING_LEVEL expectedLevelForConfigDatabase,
+            Configuration.METRICS_GATHERING_LEVEL expectedLevelForConfigCollection) {
+
+        RequestContext requestContext = createRequestContext(requestUri);
+
+        handler.configuration = configWith(OFF);
+        assertEquals(expectedLevelForConfigOff, handler.getMetricsLevelForRequest(requestContext));
+
+        handler.configuration = configWith(ROOT);
+        assertEquals(expectedLevelForConfigRoot, handler.getMetricsLevelForRequest(requestContext));
+
+        handler.configuration = configWith(DATABASE);
+        assertEquals(expectedLevelForConfigDatabase, handler.getMetricsLevelForRequest(requestContext));
+
+        handler.configuration = configWith(COLLECTION);
+        assertEquals(expectedLevelForConfigCollection, handler.getMetricsLevelForRequest(requestContext));
     }
 
     private Configuration configWith(Configuration.METRICS_GATHERING_LEVEL mgl) {
         return new Configuration() {
+
             @Override
             public METRICS_GATHERING_LEVEL getMetricsGatheringLevel() {
                 return mgl;
@@ -49,70 +99,32 @@ public class MetricsHandlerTest {
     }
 
     @Test
-    public void testGetCorrectMetricRegistry() throws Exception {
-        HttpServerExchange httpServerExchange = mock(HttpServerExchange.class);
-        when(httpServerExchange.getStatusCode()).thenReturn(200);
-        when(httpServerExchange.getRequestMethod()).thenReturn(Methods.GET);
-        when(httpServerExchange.getRequestPath()).thenReturn("/");
-
-        RequestContext requestContext = new RequestContext(httpServerExchange, "/", "/foo/bar");
-
-        handler.configuration = configWith(Configuration.METRICS_GATHERING_LEVEL.ROOT);
-        MetricRegistry registry = handler.getCorrectMetricRegistry(requestContext);
-        assertNull(registry);
-
-        handler.configuration = configWith(Configuration.METRICS_GATHERING_LEVEL.DATABASE);
-        registry = handler.getCorrectMetricRegistry(requestContext);
-        assertNull(registry);
-
-        handler.configuration = configWith(Configuration.METRICS_GATHERING_LEVEL.COLLECTION);
-        registry = handler.getCorrectMetricRegistry(requestContext);
-        assertTrue(registry instanceof MyMetricRegistry);
-        assertEquals("foo/bar", ((MyMetricRegistry) registry).getName());
-
-        when(httpServerExchange.getRequestPath()).thenReturn("/");
-        requestContext = new RequestContext(httpServerExchange, "/", "/foo");
-
-        handler.configuration = configWith(Configuration.METRICS_GATHERING_LEVEL.ROOT);
-        registry = handler.getCorrectMetricRegistry(requestContext);
-        assertNull(registry);
-
-        handler.configuration = configWith(Configuration.METRICS_GATHERING_LEVEL.DATABASE);
-        registry = handler.getCorrectMetricRegistry(requestContext);
-        assertTrue(registry instanceof MyMetricRegistry);
-        assertEquals("foo", ((MyMetricRegistry) registry).getName());
-
-        handler.configuration = configWith(Configuration.METRICS_GATHERING_LEVEL.COLLECTION);
-        registry = handler.getCorrectMetricRegistry(requestContext);
-        assertTrue(registry instanceof MyMetricRegistry);
-        assertEquals("foo", ((MyMetricRegistry) registry).getName());
-
-        when(httpServerExchange.getRequestPath()).thenReturn("/");
-        requestContext = new RequestContext(httpServerExchange, "/", "/");
-
-        handler.configuration = configWith(Configuration.METRICS_GATHERING_LEVEL.ROOT);
-        registry = handler.getCorrectMetricRegistry(requestContext);
-        assertTrue(registry instanceof MyMetricRegistry);
-        assertEquals("ROOT", ((MyMetricRegistry) registry).getName());
-
-        handler.configuration = configWith(Configuration.METRICS_GATHERING_LEVEL.DATABASE);
-        registry = handler.getCorrectMetricRegistry(requestContext);
-        assertTrue(registry instanceof MyMetricRegistry);
-        assertEquals("ROOT", ((MyMetricRegistry) registry).getName());
-
-        handler.configuration = configWith(Configuration.METRICS_GATHERING_LEVEL.COLLECTION);
-        registry = handler.getCorrectMetricRegistry(requestContext);
-        assertTrue(registry instanceof MyMetricRegistry);
-        assertEquals("ROOT", ((MyMetricRegistry) registry).getName());
+    public void testMetricsRegistryForRequestToRoot() {
+        RequestContext requestContext = createRequestContext(URI_METRICS_ROOT);
+        handler.getMetricsRegistry(requestContext, ROOT);
+        verify(handler.metrics, times(1)).registry();
     }
-    class MyMetricRegistry extends MetricRegistry {
-        String name;
-        MyMetricRegistry(String name) {
-            this.name = name;
-        }
-        
-        public String getName() {
-            return name;
-        }
+
+    @Test
+    public void testMetricsRegistryForRequestToDatabase() {
+        RequestContext requestContext = createRequestContext(URI_METRICS_DATABASE);
+        handler.getMetricsRegistry(requestContext, DATABASE);
+        verify(handler.metrics, times(1)).registry(eq(requestContext.getDBName()));
+    }
+
+    @Test
+    public void testMetricsRegistryForRequestToCollection() {
+        RequestContext requestContext = createRequestContext(URI_METRICS_COLLECTION);
+        handler.getMetricsRegistry(requestContext, COLLECTION);
+        verify(handler.metrics, times(1)).registry(eq(requestContext.getDBName()), eq(requestContext.getCollectionName()));
+    }
+
+    private RequestContext createRequestContext(String requestUri) {
+        HttpServerExchange httpServerExchange1 = mock(HttpServerExchange.class);
+        when(httpServerExchange1.getStatusCode()).thenReturn(200);
+        when(httpServerExchange1.getRequestMethod()).thenReturn(Methods.GET);
+        when(httpServerExchange1.getRequestPath()).thenReturn("/");
+        HttpServerExchange httpServerExchange = httpServerExchange1;
+        return new RequestContext(httpServerExchange, "/", requestUri);
     }
 }
