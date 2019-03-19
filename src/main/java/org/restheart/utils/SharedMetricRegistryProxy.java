095d4f1ce7f5dae93ed27f3a293e95e0198fcfd3
Christian Groth <chr.groth@gmx.de>
2019-03-19 09:43:54 +0100

    provide all metrics per database and collection for prometheus using lables on root metrics endpoint only (#340)

--- src/main/java/org/restheart/utils/SharedMetricRegistryProxy.java
+++ src/main/java/org/restheart/utils/SharedMetricRegistryProxy.java
@@ -3,18 +3,40 @@ package org.restheart.utils;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.SharedMetricRegistries;
 
+import java.util.stream.Stream;
+
 /**
  * A proxy to the shared metrics registry, mainly to make unit testing easier (extend and inject to object-under-test)
  */
 public class SharedMetricRegistryProxy {
-    private static final MetricRegistry defaultRegistry = SharedMetricRegistries.getOrCreate("default");
-    public MetricRegistry registry() {
-        return defaultRegistry;
+
+    private static final String DEFAULT_NAME = "default";
+
+    public SharedMetricRegistryProxy() {
+
+        // initialize default metrics registry name, if not already set
+        if(SharedMetricRegistries.tryGetDefault() == null) {
+            SharedMetricRegistries.setDefault(DEFAULT_NAME);
+        }
     }
+
+    public boolean isDefault(String databaseName) {
+        return DEFAULT_NAME.equals(databaseName);
+    }
+
+    public MetricRegistry registry() {
+        return SharedMetricRegistries.tryGetDefault();
+    }
+
     public MetricRegistry registry(String dbName) {
         return SharedMetricRegistries.getOrCreate(dbName);
     }
+
     public MetricRegistry registry(String dbName, String collectionName) {
         return SharedMetricRegistries.getOrCreate(dbName + "/" + collectionName);
+    }
+
+    public Stream<String> registries() {
+        return SharedMetricRegistries.names().stream();
     }
 }
