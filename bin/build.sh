6c639e020c6d3a0e930811a7c4daa915c60e0073
Maurizio Turatti <maurizio@softinstigate.com>
2019-04-17 06:22:03 -0400

    Fix build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --name my-maven-project -v "$PWD":/usr/src/app  -v "$HOME"/.m2:/root/.m2 -w /usr/src/app maven:3.6-jdk-8 mvn clean package
+mvn clean verify -DskipITs=false -Dkarate.options="$KARATE_OPS"
