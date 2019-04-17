848e7a79940b982882f12b4416bbae859fd9736b
Maurizio Turatti <maurizio@softinstigate.com>
2019-04-17 06:18:06 -0400

    build.sh reads $KARATE_OPS from env

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-set -e
-
-if [[ "$REPLICASET" == "true" ]]; then
-    mvn verify -DskipITs=false -Dkarate.options="--tags ~@requires-mongodb-4 ~@requires-replica-set"
-else
-    mvn clean verify -DskipITs=false
-fi
+mvn clean verify -DskipITs=false -Dkarate.options="$KARATE_OPS"
