28cccbbe31755aa1f8acf062c1cfc535c64bf650
Andrea Di Cesare <andrea@softinstigate.com>
2019-05-02 11:29:47 +0200

    Remove some unused imports

--- src/main/java/org/restheart/handlers/TracingInstrumentationHandler.java
+++ src/main/java/org/restheart/handlers/TracingInstrumentationHandler.java
@@ -1,23 +1,16 @@
 package org.restheart.handlers;
 
-import com.google.common.annotations.VisibleForTesting;
-
 import org.restheart.Bootstrapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 import io.undertow.server.HttpServerExchange;
-import io.undertow.util.HeaderValues;
-import io.undertow.util.HexConverter;
 import io.undertow.util.HttpString;
 
 /**
- * Handler to write tracing headers to the logging MDC. Pick it up via the default way with "%X{name}", e.g. "%X{x-b3-traceid}".
+ * Handler to write tracing headers to the logging MDC. Pick it up via the
+ * default way with "%X{name}", e.g. "%X{x-b3-traceid}".
  */
 public class TracingInstrumentationHandler extends PipedHttpHandler {
     public TracingInstrumentationHandler(PipedHttpHandler next) {
@@ -26,13 +19,13 @@ public class TracingInstrumentationHandler extends PipedHttpHandler {
 
     @Override
     public void handleRequest(HttpServerExchange exchange, RequestContext context) throws Exception {
-        for (String traceIdHeader: Bootstrapper.getConfiguration().getTraceHeaders()) {
+        for (String traceIdHeader : Bootstrapper.getConfiguration().getTraceHeaders()) {
             Optional.ofNullable(exchange.getRequestHeaders().get(traceIdHeader))
-                .flatMap(x -> Optional.ofNullable(x.peekFirst()))
-                .ifPresent(value -> {
-                    MDC.put(traceIdHeader, value);
-                    exchange.getResponseHeaders().put(HttpString.tryFromString(traceIdHeader), value);
-                });
+                    .flatMap(x -> Optional.ofNullable(x.peekFirst()))
+                    .ifPresent(value -> {
+                        MDC.put(traceIdHeader, value);
+                        exchange.getResponseHeaders().put(HttpString.tryFromString(traceIdHeader), value);
+                    });
         }
 
         if (!exchange.isResponseComplete() && getNext() != null) {
