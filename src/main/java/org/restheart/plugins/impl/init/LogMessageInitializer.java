8fefbc8734309a0a6a8f92b8eb17640a767e90c2
Andrea Di Cesare <andrea@softinstigate.com>
2019-05-09 12:56:50 +0200

    Move initializer implementation to package org.restheart.plugins.impl.init

--- src/main/java/org/restheart/plugins/impl/init/LogMessageInitializer.java
+++ src/main/java/org/restheart/plugins/impl/init/LogMessageInitializer.java
@@ -15,11 +15,13 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.restheart.plugins.init;
+package org.restheart.plugins.impl.init;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.bson.BsonDocument;
+import org.restheart.plugins.init.Initializer;
+import org.restheart.plugins.init.RegisterInitializer;
 import org.restheart.utils.LogUtils;
 import org.restheart.utils.LogUtils.Level;
 
