dc7b48beb91cdf863bd5ce63b5da4fe90652aee9
Andrea Di Cesare <andrea@softinstigate.com>
2019-05-09 13:01:20 +0200

    Move metrics logic to its own package

--- src/main/java/org/restheart/handlers/metrics/MetricsHandler.java
+++ src/main/java/org/restheart/handlers/metrics/MetricsHandler.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.restheart.handlers.applicationlogic;
+package org.restheart.handlers.metrics;
 
 import com.codahale.metrics.MetricRegistry;
 import com.google.common.annotations.VisibleForTesting;
