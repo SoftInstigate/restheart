916a6dde7b7675cd15fd908ae29ba482c9ecbf5e
Maurizio Turatti <maurizio@softinstigate.com>
2019-06-21 18:36:22 +0200

    Release 4.0.0

--- bin/maven-deploy.sh
+++ bin/maven-deploy.sh
@@ -6,8 +6,8 @@ if [[ "$MAVEN_DEPLOY" == "true" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     export RESTHEART_VERSION
     if [[ "$RESTHEART_VERSION" ]]; then
         mvn clean verify -DskipITs=false -Dkarate.options="$KARATE_OPS"
-        echo "###### Branch is '$TRAVIS_BRANCH', Tag is '$TRAVIS_TAG', Version is '$VERSION'"
-        if [[ "$TRAVIS_TAG" && "$VERSION" != *-SNAPSHOT ]]; then
+        echo "###### Branch is '$TRAVIS_BRANCH', Tag is '$TRAVIS_TAG', Version is '$RESTHEART_VERSION'"
+        if [[ "$TRAVIS_TAG" && "$RESTHEART_VERSION" != *-SNAPSHOT ]]; then
             echo "###### Building Docker image for RESTHeart $RESTHEART_VERSION"
             echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
             docker build -t "softinstigate/restheart:$RESTHEART_VERSION" .
@@ -17,8 +17,8 @@ if [[ "$MAVEN_DEPLOY" == "true" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
             echo "###### On master branch. Tagging image softinstigate/restheart:$RESTHEART_VERSION as latest"
             docker tag "softinstigate/restheart:$RESTHEART_VERSION" softinstigate/restheart:latest
             docker push softinstigate/restheart:latest
-            echo "###### Publishing Maven site at http://softinstigate.github.io/restheart/project-info.html"
-            mvn site --settings deploy-settings.xml -P report -Dmaven.test.skip=true
+            #echo "###### Publishing Maven site at http://softinstigate.github.io/restheart/project-info.html"
+            #mvn site --settings deploy-settings.xml -P report -Dmaven.test.skip=true
         fi
         echo "###### Deploy to Maven Central"
         mvn deploy -s deploy-settings.xml -P release -Dmaven.test.skip=true
