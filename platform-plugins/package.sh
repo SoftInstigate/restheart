ca9f6c133252c29d3ad18298785629d603be5128
Maurizio Turatti <maurizio@softinstigate.com>
2019-10-15 12:06:09 +0200

    Fix path

--- platform-plugins/package.sh
+++ platform-plugins/package.sh
@@ -15,7 +15,7 @@ echo "###### Packaging RESTHeart Platform $VERSION"
 echo "###### Copying files to $DIST"
 rm -rf dist
 mkdir -p "$DIST"
-cp -Rv template/ "$DIST"
+cp -Rv template/* "$DIST"
 cp -v restheart-platform-core/target/restheart-platform-core.jar "$DIST"
 cp -v restheart-platform-security/target/restheart-platform-security.jar "$DIST"
 echo "###### ...Done"
