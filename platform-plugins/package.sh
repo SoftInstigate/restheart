9f836984743dd335119b0d6c608c6afb0821a3f2
Maurizio Turatti <maurizio@softinstigate.com>
2019-10-15 11:20:52 +0200

    Move distribution files to 'dist' subfolder

--- platform-plugins/package.sh
+++ platform-plugins/package.sh
@@ -7,20 +7,21 @@ if [[ -z $VERSION ]]; then
     exit 1
 fi
 
-DIST="restheart-platform-$VERSION"
+TARGET="restheart-platform-$VERSION"
+DIST="dist/$TARGET"
 
 echo "###### Packaging RESTHeart Platform $VERSION"
 
-echo "###### Cleaning up $DIST"
-rm -rf "$DIST" "$DIST.zip"
-echo "######...Done."
-
 echo "###### Copying files to $DIST"
+rm -rf dist
+mkdir -p "$DIST"
 cp -Rv template/ "$DIST"
 cp -v restheart-platform-core/target/restheart-platform-core.jar "$DIST"
 cp -v restheart-platform-security/target/restheart-platform-security.jar "$DIST"
 echo "###### ...Done"
 
-echo "###### Compressing to archive '$DIST.zip'"
-zip -r "$DIST.zip" "$DIST" -x "*.DS_Store"
+echo "###### Compressing to archive '$TARGET.zip'"
+cd dist || exit
+zip -r "$TARGET.zip" "$TARGET" -x "*.DS_Store"
+cd ..
 echo "###### ...Done."
