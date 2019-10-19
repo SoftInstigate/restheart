90669ef2ae18b221336739cc801bdf7aedc97f9d
Andrea Di Cesare <andrea@softinstigate.com>
2019-10-19 19:54:59 +0200

    Fix license headers

--- platform-plugins/restheart-platform-security/src/main/java/com/restheart/security/plugins/authorizers/FilterPredicate.java
+++ platform-plugins/restheart-platform-security/src/main/java/com/restheart/security/plugins/authorizers/FilterPredicate.java
@@ -11,7 +11,6 @@
 package com.restheart.security.plugins.authorizers;
 
 import com.google.gson.JsonElement;
-import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import io.undertow.attribute.ExchangeAttributes;
