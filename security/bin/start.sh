a9b8a24419f2ec3b6b4c0518bd7ec45f1eaae23a
Andrea Di Cesare <andrea@softinstigate.com>
2019-10-21 16:17:27 +0200

    Improve start/stop scripts

--- security/bin/start.sh
+++ security/bin/start.sh
@@ -2,6 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-java -Dfile.encoding=UTF-8 -server -jar $DIR/../target/restheart-security.jar "$@"
+java -Dfile.encoding=UTF-8 -server -jar "$DIR/../target/restheart-security.jar" $@
 echo 'Sleeping few seconds...'
 sleep 2
