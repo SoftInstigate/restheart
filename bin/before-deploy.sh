a45d4b3012993d273c75ed5313305b32591e934c
Andrea Di Cesare <andrea@softinstigate.com>
2020-02-24 09:59:25 +0100

    Add --no-tty nad --yes to gpg -batch --fast-import to bin/before-deploy.sh

--- bin/before-deploy.sh
+++ bin/before-deploy.sh
@@ -4,6 +4,5 @@ set -e
 echo "###### TRAVIS_PULL_REQUEST==$TRAVIS_PULL_REQUEST"
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_95e9394a014a_key -iv $encrypted_95e9394a014a_iv -in bin/codesigning.asc.enc -out bin/codesigning.asc -d
-    export GPG_TTY=$(tty) // to avoid: gpg: signing failed: Inappropriate ioctl for device, ref: https://tutorials.technology/solved_errors/21-gpg-signing-failed-Inappropriate-ioctl-for-device.html
-    gpg --batch --fast-import bin/codesigning.asc
+    gpg --batch --no-tty --yes --fast-import bin/codesigning.asc
 fi
