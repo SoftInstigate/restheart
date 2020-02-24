155524a267f7d397617edc0195f75b4201ff5edb
Andrea Di Cesare <andrea@softinstigate.com>
2020-02-24 09:36:46 +0100

    Add --batch to gpg --fast-import to bin/before-deploy.sh

--- bin/before-deploy.sh
+++ bin/before-deploy.sh
@@ -4,5 +4,5 @@ set -e
 echo "###### TRAVIS_PULL_REQUEST==$TRAVIS_PULL_REQUEST"
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_95e9394a014a_key -iv $encrypted_95e9394a014a_iv -in bin/codesigning.asc.enc -out bin/codesigning.asc -d
-    gpg --fast-import bin/codesigning.asc
+    gpg --batch --fast-import bin/codesigning.asc
 fi
