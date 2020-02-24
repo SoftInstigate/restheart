ecf3cde83c922801d022cbff3b91a92c216f23bf
Andrea Di Cesare <andrea@softinstigate.com>
2020-02-24 10:23:32 +0100

    Add --pinentry-mode=loopback --batch and --yes to 'gpg --fast-import' in bin/before-deploy.sh

--- bin/before-deploy.sh
+++ bin/before-deploy.sh
@@ -4,5 +4,5 @@ set -e
 echo "###### TRAVIS_PULL_REQUEST==$TRAVIS_PULL_REQUEST"
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_95e9394a014a_key -iv $encrypted_95e9394a014a_iv -in bin/codesigning.asc.enc -out bin/codesigning.asc -d
-    gpg --pinentry-mode=loopback --fast-import bin/codesigning.asc
+    gpg --pinentry-mode=loopback --batch --yes --fast-import bin/codesigning.asc
 fi
