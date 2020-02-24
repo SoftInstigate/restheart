ee858dd46c24d48dae144dd602fb41020e0eda48
Andrea Di Cesare <andrea@softinstigate.com>
2020-02-24 10:36:07 +0100

    Add gpgArguments to maven-gpg-plugin in pom

--- bin/before-deploy.sh
+++ bin/before-deploy.sh
@@ -4,6 +4,5 @@ set -e
 echo "###### TRAVIS_PULL_REQUEST==$TRAVIS_PULL_REQUEST"
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_95e9394a014a_key -iv $encrypted_95e9394a014a_iv -in bin/codesigning.asc.enc -out bin/codesigning.asc -d
-    export GPG_TTY=$(tty)
     gpg --pinentry-mode=loopback --batch --yes --fast-import bin/codesigning.asc
 fi
