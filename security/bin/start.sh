e8a092200587d06c5075e3c6d37b07a32478eac5
Maurizio Turatti <maurizio@softinstigate.com>
2020-03-09 16:55:21 +0100

    sleep 5 seconds before CIwq

--- security/bin/start.sh
+++ security/bin/start.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 java -Dfile.encoding=UTF-8 -server -jar "$DIR/../target/restheart-security.jar" $@
 echo 'Sleeping few seconds...'
-sleep 2
+sleep 5
