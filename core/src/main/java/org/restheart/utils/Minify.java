d29d503063658a4b22f28ae45f67c62a56582319
Andrea Di Cesare <andrea@softinstigate.com>
2020-03-09 15:15:22 +0100

    Rename moudle core -> mongodb

--- core/src/main/java/org/restheart/utils/Minify.java
+++ core/src/main/java/org/restheart/utils/Minify.java
@@ -1,19 +1,19 @@
 /*
- * RESTHeart - the Web API for MongoDB
+ * RESTHeart Security
+ * 
  * Copyright (C) SoftInstigate Srl
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.restheart.utils;
 
@@ -98,9 +98,6 @@ public class Minify {
     private int line;
     private int column;
 
-    /**
-     *
-     */
     public Minify() {
         this.in = null;
         this.out = null;
@@ -417,37 +414,16 @@ public class Minify {
         return c;
     }
 
-    /**
-     *
-     */
     public static enum Action {
-
-        /**
-         *
-         */
-        OUTPUT_CURR, 
-
-        /**
-         *
-         */
-        DELETE_CURR, 
-
-        /**
-         *
-         */
-        DELETE_NEXT
+        OUTPUT_CURR, DELETE_CURR, DELETE_NEXT
     }
 
     /**
      * Exception to be thrown when an unterminated comment appears in the input.
      */
     public static class UnterminatedCommentException extends Exception {
+        private static final long serialVersionUID = -459033305952553241L;
 
-        /**
-         *
-         * @param line
-         * @param column
-         */
         public UnterminatedCommentException(int line, int column) {
             super("Unterminated comment at line " + line + " and column " + column);
         }
@@ -458,12 +434,8 @@ public class Minify {
      * input.
      */
     public static class UnterminatedStringLiteralException extends Exception {
+        private static final long serialVersionUID = -3267464275514481782L;
 
-        /**
-         *
-         * @param line
-         * @param column
-         */
         public UnterminatedStringLiteralException(int line, int column) {
             super("Unterminated string literal at line " + line + " and column " + column);
         }
@@ -474,12 +446,8 @@ public class Minify {
      * appears in the input.
      */
     public static class UnterminatedRegExpLiteralException extends Exception {
+        private static final long serialVersionUID = -998596173973803801L;
 
-        /**
-         *
-         * @param line
-         * @param column
-         */
         public UnterminatedRegExpLiteralException(int line, int column) {
             super("Unterminated regular expression at line " + line + " and column " + column);
         }
