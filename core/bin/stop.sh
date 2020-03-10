605272e1bf90d58dccc20cec3dc12b72f3e9183b
Andrea Di Cesare <andrea@softinstigate.com>
2020-03-10 13:04:18 +0100

    Fix start/stop scripts and better organizing dev.properties

--- core/bin/stop.sh
+++ core/bin/stop.sh
@@ -2,5 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-java -server -cp "$DIR/../target/restheart-security.jar" org.restheart.security.Shutdowner $@
+java -server -cp "$DIR/../target/restheart.jar" org.restheart.Shutdowner $@
 sleep 2
