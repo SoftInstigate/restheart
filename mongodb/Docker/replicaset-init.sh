5b00a68d10e053c1412c652ac7173fa65434452b
Maurizio Turatti <maurizio@softinstigate.com>
2020-03-18 18:01:07 +0100

    Delete or move old files

--- mongodb/Docker/replicaset-init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Initialize a MongoDB Replica Set with rs.initiate()
-mongo --host mongodb -u restheart --authenticationDatabase=admin -p R3ste4rt! --eval "let res = rs.status(); if (res.codeName !== 'NoReplicationEnabled') { rs.initiate(); };"
