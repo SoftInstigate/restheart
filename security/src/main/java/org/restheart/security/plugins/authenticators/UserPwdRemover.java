8236a01fb5ffb28aab2f1e24707d33bfb456bcf2
Andrea Di Cesare <andrea@softinstigate.com>
2020-06-16 12:47:06 +0200

    Fix NPE on UserPwdRemover when response content is null, this can happen when 304 is returned

--- security/src/main/java/org/restheart/security/plugins/authenticators/UserPwdRemover.java
+++ security/src/main/java/org/restheart/security/plugins/authenticators/UserPwdRemover.java
@@ -136,6 +136,7 @@ public class UserPwdRemover implements MongoInterceptor {
         return enabled
                 && request.isGet()
                 && this.usersDb.equalsIgnoreCase(request.getDBName())
-                && this.usersCollection.equalsIgnoreCase(request.getCollectionName());
+                && this.usersCollection.equalsIgnoreCase(request.getCollectionName())
+                && response.getContent() != null;
     }
 }
