cc8a8da24f9ec992b04f425ff93f4cd5654b2fce
Staant95 <antokhi.stas@gmail.com>
2021-05-27 12:35:07 +0200

    Added typescript service

---
+++ polyglot/src/test/resources/test-js-plugins/test.ts
@@ -0,0 +1,25 @@
+interface Options {
+    name: string;
+    description: string;
+    uri: string;
+    secured: boolean;
+    matchPolicy: string;
+}
+
+export const options: Options = {
+    name: "helloWorldTS",
+    description: "Test typescript",
+    uri: "/helloTS",
+    secured: false, 
+    matchPolicy: "EXACT"
+}
+
+export function handle (request: any, response: any): void {
+    // LOGGER.debug('request {}', request.getContent());
+    const rc = JSON.parse(request.getContent() || '{}');
+
+    let body = {msg: `Hello ${rc.name || 'typescript'}`}
+
+    response.setContent(JSON.stringify(body));
+    response.setContentTypeAsJson();
+}
