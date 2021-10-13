6c9c295e55c1e08d04fec74b6a72da802386ff64
stas-antokhi <stas@softinstigate.com>
2021-10-13 17:48:43 +0200

    Retrieving aggregation-time-limit from configuration

--- graphql/src/main/java/org/restheart/graphql/datafetchers/GQLAggregationDataFetcher.java
+++ graphql/src/main/java/org/restheart/graphql/datafetchers/GQLAggregationDataFetcher.java
@@ -1,11 +1,16 @@
 package org.restheart.graphql.datafetchers;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 import com.mongodb.client.AggregateIterable;
+import org.bson.BsonArray;
 import org.bson.BsonDocument;
 import org.restheart.exchange.QueryVariableNotBoundException;
 import org.restheart.graphql.models.AggregationMapping;
+import org.restheart.plugins.ConfigurationScope;
+import org.restheart.plugins.InjectConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,26 +20,30 @@ public class GQLAggregationDataFetcher extends GraphQLDataFetcher {
 
     private final Logger logger = LoggerFactory.getLogger(GQLAggregationDataFetcher.class);
 
+    private static final String AGGREGATION_TIME_LIMIT_KEY = "aggregation-time-limit";
+    private long aggregationTimeLimit;
+
+    @InjectConfiguration(scope = ConfigurationScope.ALL)
+    public void initConfig(Map<String, Object> args) {
+
+        if(args.containsKey(AGGREGATION_TIME_LIMIT_KEY)) {
+            Object limit = args.get(AGGREGATION_TIME_LIMIT_KEY);
+            if(limit instanceof Number) {
+
+                this.aggregationTimeLimit = Long.parseLong(args.get(AGGREGATION_TIME_LIMIT_KEY).toString());  
+            } else {
+                this.aggregationTimeLimit = 0;
+            }
+        }
+    }
+
+
     public GQLAggregationDataFetcher(AggregationMapping aggregationMapping) {
         super(aggregationMapping);
     }
 
     @Override
     public Object get(DataFetchingEnvironment environment) throws Exception {
-        // db = 'sample_mflix',
-        // coll = 'movies',
-        // stages = [  
-        //    { "$match": { "name": { "$var": "title" } } },  
-        // ]
-        
-        // la variabile "title" sara' settata come parametro sul campo che ha associato un aggregation mapping
-        //  e non come query param (?avars...)
-        // Es: 
-        // query {
-        //   MoviesByTomatoesRateRange(min: 3.8, max: 4.5, limit: 3, skip: 20, sort: -1) {
-        //      tomatoesRate(title: "Titanic") <--
-        //   }
-        // }
         
         return CompletableFuture.supplyAsync(() -> {
 
@@ -43,31 +52,35 @@ public class GQLAggregationDataFetcher extends GraphQLDataFetcher {
             try {
                 aggregation.getResolvedStagesAsList(environment);
             } catch (QueryVariableNotBoundException e) {
-                // TODO Auto-generated catch block
+                logger.info("Something went wrong while trying to resolve stages {}", e.getMessage());
                 e.printStackTrace();
             }
 
             AggregateIterable<BsonDocument> res = null;
             try {
+                var aggregationList =  aggregation.getResolvedStagesAsList(environment);
                 res = mongoClient
                         .getDatabase(aggregation.getDb().getValue())
                         .getCollection(aggregation.getCollection().getValue())
                         .withDocumentClass(BsonDocument.class)
                         .aggregate(
-                            aggregation.getResolvedStagesAsList(environment)
+                            aggregationList
                         )
-                        .allowDiskUse(aggregation.getAllowDiskUse().getValue());
-                        // .maxTime(MongoServiceConfiguration.get()
-                        // .getAggregationTimeLimit(), TimeUnit.MILLISECONDS);
+                        .allowDiskUse(aggregation.getAllowDiskUse().getValue())
+                        .maxTime(this.aggregationTimeLimit, TimeUnit.MILLISECONDS);
+
             } catch (QueryVariableNotBoundException e) {
-                // TODO Auto-generated catch block
+                logger.error("Aggregation pipeline has failed! {}", e.getMessage());
                 e.printStackTrace();
             }
-            if(res.iterator().hasNext()) {
-                this.logger.info("first {}", res.first());
-            }
+
+            var stageOutput = new BsonArray();
             
-            return res.iterator().hasNext() ? res.first() : null;
+            if(res != null) {
+                res.forEach(doc -> stageOutput.add(doc));
+            }
+
+            return stageOutput;
         });
     }
     
