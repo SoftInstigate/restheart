ee14052fc56aeb149281f2ae3323d3e2ac835aa3
Andrea Di Cesare <andrea@softinstigate.com>
2022-01-03 13:28:06 +0100

    :white_check_mark: Update javascript test plugins to show how to use BsonUtils.array() and BsonUtils.document()
    (cherry picked from commit 7556de286b5984458f7c17443a8bb6b32711e442)

--- polyglot/src/test/resources/test-js-plugins/hello.js
+++ polyglot/src/test/resources/test-js-plugins/hello.js
@@ -1,3 +1,5 @@
+const BsonUtils = Java.type("org.restheart.utils.BsonUtils");
+
 export const options = {
     name: "helloWorldService",
     description: "just another Hello World",
@@ -7,6 +9,8 @@ export const options = {
 }
 
 export function handle(request, response) {
+    exampleBsonUtils();
+
     LOGGER.debug('request {}', request.getContent());
     const rc = JSON.parse(request.getContent() || '{}');
 
@@ -16,4 +20,19 @@ export function handle(request, response) {
 
     response.setContent(JSON.stringify(body));
     response.setContentTypeAsJson();
+}
+
+/**
+ * this snippet shows how to use BsonUtils.document() and BsonUtils.array()
+ */
+function exampleBsonUtils() {
+    // _array is a Java BsonArray with two BsonDocument elements
+    const _array = BsonUtils.array().add(
+        BsonUtils.document().put("foo", 1).put("bar", 2),
+        BsonUtils.document().put("foo", 1).put("bar", 2));
+    // turn the java object to a javascript object
+    // parsing the string representation of _array (got via .toJson())
+    const array = JSON.parse(_array.toJson());
+    // log it
+    LOGGER.info('Example BSON array built with BsonUtils {}', array);
 }
