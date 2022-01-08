96f05b708792cdd8a513cf50927a0c36a8b11554
Andrea Di Cesare <andrea@softinstigate.com>
2022-01-08 16:04:01 +0100

    :sparkles: Better handle MongoException with error codes 56 and 40353 (FieldPath must not end with a '.')
    (cherry picked from commit d50962e930fe32d69e8fad4a7f3ed2b1f6b77821)

--- mongodb/src/main/java/org/restheart/mongodb/utils/ResponseHelper.java
+++ mongodb/src/main/java/org/restheart/mongodb/utils/ResponseHelper.java
@@ -51,14 +51,12 @@ public class ResponseHelper {
      * @param exchange
      * @param properties
      */
-    public static void injectEtagHeader(
-            HttpServerExchange exchange,
-            BsonDocument properties) {
+    public static void injectEtagHeader(HttpServerExchange exchange, BsonDocument properties) {
         if (properties == null) {
             return;
         }
 
-        BsonValue _etag = properties.get("_etag");
+        var _etag = properties.get("_etag");
 
         if (_etag == null) {
             return;
@@ -76,14 +74,12 @@ public class ResponseHelper {
      * @param exchange
      * @param properties
      */
-    public static void injectEtagHeader(
-            HttpServerExchange exchange,
-            Document properties) {
+    public static void injectEtagHeader(HttpServerExchange exchange, Document properties) {
         if (properties == null) {
             return;
         }
 
-        Object _etag = properties.get("_etag");
+        var _etag = properties.get("_etag");
 
         if (_etag == null) {
             return;
@@ -91,8 +87,8 @@ public class ResponseHelper {
 
         if (_etag instanceof ObjectId) {
             setETagHeader(exchange, _etag.toString());
-        } else if (_etag instanceof String) {
-            setETagHeader(exchange, (String) _etag);
+        } else if (_etag instanceof String setag) {
+            setETagHeader(exchange, setag);
         }
     }
 
@@ -101,17 +97,13 @@ public class ResponseHelper {
      * @param exchange
      * @param etag
      */
-    public static void injectEtagHeader(
-            HttpServerExchange exchange,
-            Object etag) {
+    public static void injectEtagHeader(HttpServerExchange exchange, Object etag) {
 
         if (etag == null) {
             return;
         }
 
-        if (etag instanceof BsonValue) {
-            BsonValue _etag = (BsonValue) etag;
-
+        if (etag instanceof BsonValue _etag) {
             if (_etag.isObjectId()) {
                 setETagHeader(exchange, _etag.asObjectId().getValue().toString());
             } else if (_etag.isString()) {
@@ -132,50 +124,36 @@ public class ResponseHelper {
      * @return
      */
     public static int getHttpStatusFromErrorCode(int code) {
-        switch (code) {
-            case 2:
-                // Unknown top level operator
-                return HttpStatus.SC_BAD_REQUEST;
-            case 13:
-                // The MongoDB user does not have enough permissions to execute this operation.
-                return HttpStatus.SC_FORBIDDEN;
-            case 18:
-                // Wrong MongoDB user credentials
-                return HttpStatus.SC_FORBIDDEN;
-            case 61:
-                // Write request for sharded collection must specify the shardkey.
-                return HttpStatus.SC_BAD_REQUEST;
-            case 66:
-                // Update tried to change the immutable shardkey
-                return HttpStatus.SC_FORBIDDEN;
-            case 121:
-                // Document failed validation
-                return HttpStatus.SC_BAD_REQUEST;
-            case 112:
-                //WriteConflict
-                return HttpStatus.SC_CONFLICT;
-            case 225:
+        return switch (code) {
+            // Unknown top level operator
+            case 2 -> HttpStatus.SC_BAD_REQUEST;
+            // The MongoDB user does not have enough permissions to execute this operation.
+            case 13 -> HttpStatus.SC_FORBIDDEN;
+            // Wrong MongoDB user credentials
+            case 18 -> HttpStatus.SC_FORBIDDEN;
+            // Write request for sharded collection must specify the shardkey.
+            case 61 -> HttpStatus.SC_BAD_REQUEST;
+            // Update tried to change the immutable shardkey
+            case 66 -> HttpStatus.SC_FORBIDDEN;
+            // Document failed validation
+            case 121 -> HttpStatus.SC_BAD_REQUEST;
+            //WriteConflict
+            case 112 -> HttpStatus.SC_CONFLICT;
             // Cannot start transaction X on session Y because a newer transaction Z has already started
-            case 251:
             // transaction number X does not match any in-progress transactions
-            case 256:
-                // Transaction X has been committed.
-                return HttpStatus.SC_NOT_ACCEPTABLE;
-            case 11000:
-                // error 11000 is duplicate key error
-                // happens when the _id and a filter are specified,
-                // the document exists but does not match the filter
-                return HttpStatus.SC_CONFLICT;
-            case 13297:
-                // db already exists with different case
-                return HttpStatus.SC_CONFLICT;
-            case 51091:
-                //
-                return HttpStatus.SC_BAD_REQUEST;
-            default:
-                // Other
-                return HttpStatus.SC_INTERNAL_SERVER_ERROR;
-        }
+            // Transaction X has been committed.
+            case 225,251,256 -> HttpStatus.SC_NOT_ACCEPTABLE;
+            // error 11000 is duplicate key error
+            // happens when the _id and a filter are specified,
+            // the document exists but does not match the filter
+            case 11000 ->HttpStatus.SC_CONFLICT;
+            // db already exists with different case
+            case 13297 -> HttpStatus.SC_CONFLICT;
+            // FieldPath must not end with a '.'
+            case 56, 40353 -> HttpStatus.SC_BAD_REQUEST;
+            case 51091 -> HttpStatus.SC_BAD_REQUEST;
+            default -> HttpStatus.SC_INTERNAL_SERVER_ERROR;
+        };
     }
 
     /**
@@ -212,13 +190,8 @@ public class ResponseHelper {
     public static String getMessageFromErrorCode(int code) {
         return switch(code) {
             case 13 -> "The MongoDB user does not have enough permissions to execute this operation";
-            case 18 -> "Wrong MongoDB user credentials "
-                        + "(wrong password or need to specify the "
-                        + "authentication dababase "
-                        + "with 'authSource=<db>' option in mongo-uri)";
-            case 61 -> "Write request for sharded "
-                        + "collection must specify the shardkey. "
-                        + "Use the 'shardkey' query parameter";
+            case 18 -> "Wrong MongoDB user credentials (wrong password or need to specify the authentication dababase with 'authSource=<db>' option in mongo-uri)";
+            case 61 -> "Write request for sharded collection must specify the shardkey. Use the 'shardkey' query parameter";
             case 66 -> "Update tried to change the immutable shardkey";
             case 121 -> "Document failed validation";
             case 112 -> "Write conflict inside transaction";
@@ -234,6 +207,8 @@ public class ResponseHelper {
             // - for upsert/updates with the _id and a filter, and the update document does not match the filter
             // - a unique index exists, and the document duplicates an existing key
             case 11000 -> "Duplicate key error (insert with existing _id, update a document not matching specified filter or unique index violation)";
+            // FieldPath must not end with a '.'
+            case 56, 40353 -> "FieldPath must not end with a '.'";
             case 13297 -> "Db already exists with different case";
             default -> "Error handling the request, see log for more information";
         };
