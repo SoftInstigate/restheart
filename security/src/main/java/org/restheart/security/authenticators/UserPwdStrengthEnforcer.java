59b5d5a5b9025589d335fbcd505b7b0d518dc67e
Andrea Di Cesare <andrea@softinstigate.com>
2022-03-15 11:35:45 +0100

    :sparkles: MongoRealmAuthenticator can check the password field on user document updates and reject it when it is too weak

--- security/src/main/java/org/restheart/security/authenticators/UserPwdStrengthEnforcer.java
+++ security/src/main/java/org/restheart/security/authenticators/UserPwdStrengthEnforcer.java
@@ -20,42 +20,54 @@
  */
 package org.restheart.security.authenticators;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.jayway.jsonpath.JsonPath;
-import com.jayway.jsonpath.PathNotFoundException;
-import org.bson.BsonString;
-import org.mindrot.jbcrypt.BCrypt;
 import org.restheart.ConfigurationException;
 import org.restheart.exchange.MongoRequest;
 import org.restheart.exchange.MongoResponse;
+import static org.restheart.plugins.InterceptPoint.REQUEST_AFTER_AUTH;
+
+import java.util.Spliterators;
+import java.util.stream.StreamSupport;
+
 import org.restheart.plugins.InjectPluginsRegistry;
-import org.restheart.plugins.InterceptPoint;
 import org.restheart.plugins.MongoInterceptor;
 import org.restheart.plugins.PluginRecord;
 import org.restheart.plugins.PluginsRegistry;
 import org.restheart.plugins.RegisterPlugin;
 import org.restheart.plugins.security.Authenticator;
 import org.restheart.utils.BsonUtils;
+import static org.restheart.utils.BsonUtils.document;
+import static org.restheart.utils.BsonUtils.array;
+import org.restheart.utils.HttpStatus;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
+import com.nulabinc.zxcvbn.Feedback;
+import com.nulabinc.zxcvbn.Zxcvbn;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * helper interceptor to add token headers to Access-Control-Expose-Headers to
+ * handle CORS request
  *
  * @author Andrea Di Cesare {@literal <andrea@softinstigate.com>}
  */
-@RegisterPlugin(name = "userPwdHasher",
-        description = "automatically hashes the user password",
-        interceptPoint = InterceptPoint.REQUEST_AFTER_AUTH,
-        requiresContent = true)
-public class UserPwdHasher implements MongoInterceptor {
+@RegisterPlugin(name="userPwdStrengthEnforcer",
+        description = "enforce strong password for mongoRealmAuthenticator",
+        interceptPoint = REQUEST_AFTER_AUTH,
+        enabledByDefault = true,
+        priority = Integer.MIN_VALUE)
+public class UserPwdStrengthEnforcer implements MongoInterceptor {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserPwdStrengthEnforcer.class);
 
-    static final Logger LOGGER = LoggerFactory.getLogger(UserPwdHasher.class);
+    private static Zxcvbn zxcvbn = new Zxcvbn();
 
     private String usersDb;
     private String usersCollection;
     private String propNamePassword;
-    private Integer complexity;
+    private Integer minimumPasswordStrength;
 
     private boolean enabled = false;
 
@@ -75,7 +87,7 @@ public class UserPwdHasher implements MongoInterceptor {
         } else {
             var rhAuth = (MongoRealmAuthenticator) _mra.getInstance();
 
-            if (!rhAuth.isBcryptHashedPassword()) {
+            if (!rhAuth.isEnforceMinimumPasswordStrenght()) {
                 this.enabled = false;
                 return;
             }
@@ -83,18 +95,18 @@ public class UserPwdHasher implements MongoInterceptor {
             this.usersDb = rhAuth.getUsersDb();
             this.usersCollection = rhAuth.getUsersCollection();
             this.propNamePassword = rhAuth.getPropPassword();
-            this.complexity = rhAuth.getBcryptComplexity();
+            this.minimumPasswordStrength = rhAuth.getMinimumPasswordStrength();
 
             if (usersDb == null
                     || usersCollection == null
                     || propNamePassword == null
-                    || complexity == null) {
+                    || minimumPasswordStrength == null) {
                 LOGGER.error("Wrong configuration of mongoRealmAuthenticator! "
                         + "Password field of users documents "
-                        + "is not automatically entcrypted: "
+                        + "is not automatically checked for password strenght: "
                         + "{usersDb: {}, usersCollection: {}, "
-                        + "propNamePassword: {}, complexity: {}})",
-                        usersDb, usersCollection, propNamePassword, complexity);
+                        + "propNamePassword: {}, minimumPasswordStrength: {}})",
+                        usersDb, usersCollection, propNamePassword, minimumPasswordStrength);
                 enabled = false;
             } else {
                 enabled = true;
@@ -114,25 +126,33 @@ public class UserPwdHasher implements MongoInterceptor {
 
             int[] iarr = {0};
 
-            passwords.forEach(plain -> {
+            StreamSupport.stream(Spliterators.spliteratorUnknownSize(passwords.iterator(), 0), false)
+                .takeWhile(p -> !response.isInError())
+                .forEach(plain -> {
                 if (plain != null && plain.isJsonPrimitive() && plain.getAsJsonPrimitive().isString()) {
-                    var hashed = BCrypt.hashpw(plain.getAsJsonPrimitive().getAsString(), BCrypt.gensalt(complexity));
+                    var password = plain.getAsJsonPrimitive().getAsString();
+                    var measure = zxcvbn.measure(password);
 
-                    content.asArray().get(iarr[0]).asDocument().put(this.propNamePassword, new BsonString(hashed));
+                    if (measure.getScore()  < this.minimumPasswordStrength) {
+                        reject(response, measure.getFeedback(), iarr[0]);
+                    }
                 }
 
                 iarr[0]++;
             });
         } else if (content.isDocument()) {
             // PUT/PATCH document or bulk PATCH
-            JsonElement plain;
             try {
-                plain = JsonPath.read(BsonUtils.toJson(content), "$.".concat(this.propNamePassword));
+                JsonElement plain = JsonPath.read(BsonUtils.toJson(content), "$.".concat(this.propNamePassword));
 
                 if (plain != null && plain.isJsonPrimitive() && plain.getAsJsonPrimitive().isString()) {
-                    String hashed = BCrypt.hashpw(plain.getAsJsonPrimitive().getAsString(), BCrypt.gensalt(complexity));
+                    var password = plain.getAsJsonPrimitive().getAsString();
 
-                    content.asDocument().put(this.propNamePassword, new BsonString(hashed));
+                    var measure = zxcvbn.measure(password);
+
+                    if (measure.getScore()  < this.minimumPasswordStrength) {
+                        reject(response, measure.getFeedback());
+                    }
                 }
             } catch (PathNotFoundException pnfe) {
                 return;
@@ -140,13 +160,42 @@ public class UserPwdHasher implements MongoInterceptor {
         }
     }
 
+    private void reject(MongoResponse response, Feedback feedback) {
+        reject(response, feedback, null);
+    }
+
+    private void reject(MongoResponse response, Feedback feedback, Integer idx) {
+        var error = document()
+            .put("message", idx == null ? "Password is too weak" : "Password is too weak in user document at index " + idx)
+            .put("http status code", HttpStatus.SC_NOT_ACCEPTABLE)
+            .put("http status description", HttpStatus.getStatusText(HttpStatus.SC_NOT_ACCEPTABLE));
+
+        var warning = feedback.getWarning();
+
+        if (warning != null && !warning.isEmpty()) {
+            error.put("warning", warning);
+        }
+
+        var suggestions = feedback.getSuggestions();
+
+        if (suggestions != null && !suggestions.isEmpty()) {
+            var _suggestions = array();
+            suggestions.stream().forEach(suggestion -> _suggestions.add(suggestion.toString()));
+            error.put("suggestions", _suggestions);
+        }
+
+        response.setContent(error);
+        response.setStatusCode(HttpStatus.SC_NOT_ACCEPTABLE);
+        response.setInError(true);
+    }
+
     @Override
     public boolean resolve(MongoRequest request, MongoResponse response) {
         return enabled
-                && request.isHandledBy("mongo")
-                && request.isWriteDocument()
-                && request.isContentTypeJson()
-                && this.usersDb.equalsIgnoreCase(request.getDBName())
-                && this.usersCollection.equalsIgnoreCase(request.getCollectionName());
+            && request.isHandledBy("mongo")
+            && request.isWriteDocument()
+            && request.isContentTypeJson()
+            && this.usersDb.equalsIgnoreCase(request.getDBName())
+            && this.usersCollection.equalsIgnoreCase(request.getCollectionName());
     }
 }
