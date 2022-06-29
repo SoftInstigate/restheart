c2104195d8fb199d59dc885464bfc3229917a6cf
Maurizio Turatti <maurizio@softinstigate.com>
2022-06-29 15:13:56 +0200

    Release 6.3.5

--- setversion.sh
+++ setversion.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+set -Eeuo pipefail
+
+echo "Setting new version to $1"
+
+on_error() {
+    echo "Reverting to $VERSION"
+    mvn versions:set -DnewVersion="$VERSION"
+    exit 1
+}
+
+trap on_error ERR
+
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+echo "Old version was $VERSION"
+
 mvn versions:set -DnewVersion="$1"
+mvn clean compile
+#git commit -am "Release $1"
