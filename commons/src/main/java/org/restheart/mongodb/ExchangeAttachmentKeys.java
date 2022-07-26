eafd170e0c8eeb9421b9ca59599de359300142fc
Andrea Di Cesare <andrea@softinstigate.com>
2022-07-26 16:12:22 +0200

    :page_facing_up: Add missing license header

---
+++ commons/src/main/java/org/restheart/mongodb/ExchangeAttachmentKeys.java
@@ -0,0 +1,54 @@
+/*-
+ * ========================LICENSE_START=================================
+ * restheart-mongodb
+ * %%
+ * Copyright (C) 2014 - 2022 SoftInstigate
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
+
+package org.restheart.mongodb;
+
+import org.bson.BsonValue;
+
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.AttachmentKey;
+
+/**
+ * Util class for exchange attachments related to the MongoService
+ *
+ * @author Andrea Di Cesare {@literal <andrea@softinstigate.com>}
+ */
+public class ExchangeAttachmentKeys {
+    private static final AttachmentKey<BsonValue> MONGO_REQUEST_CONTENT_KEY = AttachmentKey.create(BsonValue.class);
+
+    /**
+     *
+     * @param exchange
+     * @return the BsonValue attached to the exchange
+     */
+    public static BsonValue attachedBsonContent(HttpServerExchange exchange) {
+        return exchange.getAttachment(MONGO_REQUEST_CONTENT_KEY);
+    }
+
+    /**
+     * set the intialized flag for MongoRequest
+     *
+     * @param exchange
+     * @param value the BsonValue to attache to the exchange
+     */
+    public static void attachBsonContent(HttpServerExchange exchange, BsonValue value) {
+        exchange.putAttachment(MONGO_REQUEST_CONTENT_KEY, value);
+    }
+}
---
+++ examples/protobuffer-contacts/src/test/java/org/restheart/examples/XxxTest.java
@@ -0,0 +1,31 @@
+package org.restheart.examples;
+
+import com.mongodb.MongoClientSettings;
+
+import org.bson.BsonDocument;
+import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.junit.Test;
+
+import io.github.gaplotech.PBCodecProvider;
+
+public class XxxTest {
+    @Test
+    public void testCodec() {
+        var registry = CodecRegistries.fromRegistries(
+            CodecRegistries.fromProviders(new PBCodecProvider()),
+            MongoClientSettings.getDefaultCodecRegistry());
+
+        var contact = ContactPostRequest.newBuilder()
+            .setName("name")
+            .setEmail("email")
+            .setPhone("phone")
+            .build();
+
+        var doc = new Document().append("key", contact);
+
+        var bson = doc.toBsonDocument(BsonDocument.class, registry).get("key").asDocument();
+
+        System.out.println(bson.toJson());
+    }
+}
