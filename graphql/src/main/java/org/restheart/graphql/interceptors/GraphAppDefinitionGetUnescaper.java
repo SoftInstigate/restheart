a1e7f9366494ec9f9526128c043cf02dcb1061e1
Andrea Di Cesare <andrea@softinstigate.com>
2022-09-21 14:52:43 +0200

    :recycle: Renamed interceptor

--- graphql/src/main/java/org/restheart/graphql/interceptors/GraphAppDefinitionGetUnescaper.java
+++ graphql/src/main/java/org/restheart/graphql/interceptors/GraphAppDefinitionGetUnescaper.java
@@ -37,12 +37,12 @@ import org.restheart.utils.BsonUtils;
 import java.util.Map;
 
 
-@RegisterPlugin(name="graphAppDefinitionPutPostEscaper",
+@RegisterPlugin(name="gaphAppDefinitionGetUnescaper",
         description = "unescapes $ prefixed keys in GraphQL application definitions",
         interceptPoint = RESPONSE,
         enabledByDefault = true
 )
-public class GraphAppDefinitionPutPostEscaper implements MongoInterceptor {
+public class GraphAppDefinitionGetUnescaper implements MongoInterceptor {
     private String db = GraphQLService.DEFAULT_APP_DEF_DB;
     private String coll = GraphQLService.DEFAULT_APP_DEF_COLLECTION;
 
