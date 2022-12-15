637a06169b7f53df3ca79edf1aba0cc2acb3a505
Andrea Di Cesare <andrea@softinstigate.com>
2022-12-15 18:14:57 +0100

    :white_check_mark: Add test for GraphQL typeResolver predicates

--- graphql/src/test/java/org/restheart/graphql/predicates/PredicatesTest.java
+++ graphql/src/test/java/org/restheart/graphql/predicates/PredicatesTest.java
@@ -11,18 +11,42 @@ import static org.restheart.utils.BsonUtils.document;
 
 public class PredicatesTest {
     @Test
-    public void testExistsUndertow() {
-        var predicate = "x-exists(foo) or x-exists(bar)";
-        var t = document().put("foo", 1).get();
-        var f = document().put("bar", 1).get();
+    public void testDocContains() {
+        var fooOrBar = "doc-contains(foo) or doc-contains(bar)";
+        var fooAndBar = "doc-contains(foo, bar)";
 
-        var _predicate = Predicates.parse(predicate);
+        var fooDoc = document().put("foo", 1).get();
+        var barDoc = document().put("bar", 1).get();
+        var fooAndBarDoc = document()
+            .put("bar", 1)
+            .put("foo", 1).get();
 
-        assertTrue(null, _predicate.resolve(DocInExchange.exchange(t)));
-        assertTrue(null, _predicate.resolve(DocInExchange.exchange(f)));
+        var _fooOrBar = Predicates.parse(fooOrBar);
+        var _fooAndBar = Predicates.parse(fooAndBar);
+
+        assertTrue(_fooOrBar.resolve(DocInExchange.exchange(fooDoc)));
+        assertTrue(_fooOrBar.resolve(DocInExchange.exchange(barDoc)));
+
+        assertFalse(_fooAndBar.resolve(DocInExchange.exchange(fooDoc)));
+        assertFalse(_fooAndBar.resolve(DocInExchange.exchange(barDoc)));
+        assertTrue(_fooAndBar.resolve(DocInExchange.exchange(fooAndBarDoc)));
+    }
+
+    @Test
+    public void testDocFieldEq() {
+        var fooEqOne = "doc-field-eq(field=foo, value=1)";
+        var barEqObj = "doc-field-eq(field=bar, value='{\"a\":1}')";
+
+        var fooDoc = document().put("foo", 1).get();
+        var barDoc = document().put("bar", document().put("a", 1)).get();
+
+        var _fooEqOne = Predicates.parse(fooEqOne);
+        var _barEqObj = Predicates.parse(barEqObj);
+
+        assertTrue(_barEqObj.resolve(DocInExchange.exchange(barDoc)));
+        assertTrue(_fooEqOne.resolve(DocInExchange.exchange(fooDoc)));
+
+        assertFalse(_barEqObj.resolve(DocInExchange.exchange(fooDoc)));
+        assertFalse(_fooEqOne.resolve(DocInExchange.exchange(barDoc)));
     }
 }
-
-
-
-
