24500fa1a3ceb3a0ea0973be2bd3e31f575b711d
Andrea Di Cesare <andrea@softinstigate.com>
2023-02-11 17:18:53 +0100

    :recycle: Refactor CacheInvalidator

--- mongodb/src/main/java/org/restheart/mongodb/services/CacheInvalidator.java
+++ mongodb/src/main/java/org/restheart/mongodb/services/CacheInvalidator.java
@@ -20,7 +20,6 @@
  */
 package org.restheart.mongodb.services;
 
-import java.util.Deque;
 import org.restheart.exchange.ByteArrayRequest;
 import org.restheart.exchange.ByteArrayResponse;
 import org.restheart.mongodb.MongoServiceConfiguration;
@@ -33,11 +32,9 @@ import org.restheart.utils.HttpStatus;
  *
  * @author Andrea Di Cesare {@literal <andrea@softinstigate.com>}
  */
-@RegisterPlugin(name = "cacheInvalidator",
-        description = "Invalidates the db and collection metadata cache",
-        defaultURI = "/ic"
-)
+@RegisterPlugin(name = "cacheInvalidator", description = "Invalidates the MongoService db and collection metadata cache", defaultURI = "/ic")
 public class CacheInvalidator implements ByteArrayService {
+    private static MetadataCachesSingleton CACHE = MetadataCachesSingleton.getInstance();
 
     /**
      *
@@ -45,41 +42,32 @@ public class CacheInvalidator implements ByteArrayService {
      */
     @Override
     public void handle(ByteArrayRequest request, ByteArrayResponse response) throws Exception {
-        var exchange = request.getExchange();
-
         if (!MongoServiceConfiguration.get().isLocalCacheEnabled()) {
-            response.setInError(
-                    HttpStatus.SC_NOT_MODIFIED,
-                    "caching is off");
+            response.setInError(HttpStatus.SC_NOT_MODIFIED, "caching is off");
             return;
         }
 
         if (request.isOptions()) {
             handleOptions(request);
-        } else if (request.isPost()) {
-            Deque<String> _db = exchange.getQueryParameters().get("db");
-            Deque<String> _coll = exchange.getQueryParameters().get("coll");
-
-            if (_db == null || _db.getFirst() == null) {
-                response.setInError(
-                        HttpStatus.SC_BAD_REQUEST,
-                        "the db query paramter is mandatory");
-            } else {
-                String db = _db.getFirst();
-
-                if (_coll == null || _coll.getFirst() == null) {
-                    MetadataCachesSingleton.getInstance().invalidateDb(db);
-                } else {
-                    String coll = _coll.getFirst();
-
-                    MetadataCachesSingleton.getInstance()
-                            .invalidateCollection(db, coll);
-                }
-
-                response.setStatusCode(HttpStatus.SC_OK);
-            }
-        } else {
-            response.setStatusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
+            return;
         }
+
+        if (!request.isPost()) {
+            response.setStatusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
+            return;
+        }
+
+        var db = request.getQueryParameterOfDefault("db", null);
+        var coll = request.getQueryParameterOfDefault("coll", null);
+
+        if (db == null) {
+            response.setInError(HttpStatus.SC_BAD_REQUEST, "the db query paramter is mandatory");
+        } else if (coll == null) {
+            CACHE.invalidateDb(db);
+        } else {
+            CACHE.invalidateCollection(db, coll);
+        }
+
+        response.setStatusCode(HttpStatus.SC_OK);
     }
 }
