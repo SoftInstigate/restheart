336cfc148c9c8f43d41e14894c2f11ef2649f194
Maurizio Turatti <maurizio@softinstigate.com>
2023-03-23 16:46:34 +0100

    Add a help message [skip ci]

--- setversion.sh
+++ setversion.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+if [ -z "$1" ]
+then
+cat << EOF
+This script is used to set the version of the project.
+It also commits the change and creates a tag if the version is not a SNAPSHOT.
+To create a new stable release of RESTHeart, for example 7.3.4:
+
+   $ ./setversion.sh 7.3.4
+   $ ./setversion.sh 7.3.5-SNAPSHOT
+   $ git push && git push --tags
+
+EOF
+exit 1
+fi
+
 set -Eeuo pipefail
 
 mvn versions:set -DnewVersion="$1"
