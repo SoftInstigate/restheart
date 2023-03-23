021d4fb38f0e716fd9e02adcb5dd32bafeb2db31
Maurizio Turatti <maurizio@softinstigate.com>
2023-03-23 16:10:55 +0100

    Release version 7.3.3

--- setversion.sh
+++ setversion.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+set -Eeuo pipefail
+
 mvn versions:set -DnewVersion="$1"
+
+if [[ "$1" == *SNAPSHOT ]]
+then
+  echo "This is a SNAPSHOT";
+  git commit -am "Bump version to $1 [skip ci]"
+else
+  echo "This is a Release";
+  git commit -am "Release version $1"
+  git tag "$1"
+fi
