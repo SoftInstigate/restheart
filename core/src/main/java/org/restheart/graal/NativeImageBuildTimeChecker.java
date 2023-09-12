e0fd279d5a1a1ce8823f9fa008ae3c6bc7c55e7e
Andrea Di Cesare <andrea@softinstigate.com>
2023-09-12 16:03:36 +0200

    :arrow_up: Upgrade GraalVM to v23.0.1 (17.0.8-graal)

--- core/src/main/java/org/restheart/graal/NativeImageBuildTimeChecker.java
+++ core/src/main/java/org/restheart/graal/NativeImageBuildTimeChecker.java
@@ -23,11 +23,11 @@ package org.restheart.graal;
 /**
  * this is used by PluginsScanner to determine if scanning is occurring at build
  * or run time. When running with java, Bootstrapper calls
- * NativeImageBuildTimeChecker.atRunting() making the variable bt=false. When
+ * NativeImageBuildTimeChecker.atRuntime() setting the variable bt=false. When
  * building with native-image, the class is initialized at build time (due to
  * native-image.properties that sets PluginScanner to be initialized at build
  * time and tranistively NativeImageBuildTimeChecker, since PluginScanner
- * references it) making bt=false
+ * references it) with bt=true
  */
 public class NativeImageBuildTimeChecker {
     private static boolean bt = true;
