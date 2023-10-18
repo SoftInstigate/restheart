90ac83278723032aba1ef10e0e244c7bb8c481a4
Andrea Di Cesare <andrea@softinstigate.com>
2023-10-18 17:54:23 +0200

    :bug: Fix DocumentBuilder.put(String key, Instant value)

--- graphql/src/main/java/org/restheart/graphql/datafetchers/GQLAggregationDataFetcher.java
+++ graphql/src/main/java/org/restheart/graphql/datafetchers/GQLAggregationDataFetcher.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * =========================LICENSE_END==================================
@@ -21,8 +21,6 @@
 
 package org.restheart.graphql.datafetchers;
 
-import java.util.ArrayList;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import com.mongodb.client.AggregateIterable;
