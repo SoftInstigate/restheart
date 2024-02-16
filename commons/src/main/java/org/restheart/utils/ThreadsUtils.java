85e6b31518ae0de4244360c9f3d92b821b43fb25
Andrea Di Cesare <andrea@softinstigate.com>
2024-02-16 13:13:31 +0100

    :sparkles: Use virtual threads for executing worker threads [skip ci]
    https://github.com/SoftInstigate/restheart/issues/474

---
+++ commons/src/main/java/org/restheart/utils/ThreadsUtils.java
@@ -0,0 +1,33 @@
+/*-
+ * ========================LICENSE_START=================================
+ * restheart-core
+ * %%
+ * Copyright (C) 2014 - 2023 SoftInstigate
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * =========================LICENSE_END==================================
+ */
+
+package org.restheart.utils;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class ThreadsUtils {
+    private static final Executor VIRTUAL_THREAD_EXECUTOR = Executors.newThreadPerTaskExecutor(Thread.ofVirtual().name("RH-VIRTUAL-WORKER-", 0L).factory());
+
+    public static Executor virtualThreadsExecutor() {
+        return VIRTUAL_THREAD_EXECUTOR;
+    }
+}
