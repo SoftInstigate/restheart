31bd51eabb13de4ddc8e6933556cc3d10d8a908b
Maurizio Turatti <maurizio@softinstigate.com>
2024-07-15 20:37:53 +0200

    Add an optional shade profile to explicitly create the fat jar

--- core/bin/start.sh
+++ core/bin/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-java -Dfile.encoding=UTF-8 -server -jar "$DIR/../target/restheart.jar" $@
+# Run the fat jar
+#java -Dfile.encoding=UTF-8 -server -jar "$DIR/../target/restheart.jar" $@
+
+# run the regular jar with complete classpath
+java -Dfile.encoding=UTF-8 -server \
+    -cp "$DIR/../target/restheart-core.jar:$DIR/../target/lib/*:$DIR/../target/plugins/*" \
+    org.restheart.Bootstrapper $@
--- core/bin/stop.sh
+++ core/bin/stop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-java -server -cp "$DIR/../target/restheart.jar" org.restheart.Shutdowner $@
+# stop the fat jar
+#java -server -cp "$DIR/../target/restheart.jar" org.restheart.Shutdowner $@
+
+# stop the regular jar with complete classpath
+java -server \
+    -cp "$DIR/../target/restheart-core.jar:$DIR/../target/lib/*:$DIR/../target/plugins/*" \
+    org.restheart.Shutdowner $@
