68d8b3bb468dea782804063aafbc7efabb54e76c
Maurizio Turatti <maurizio@softinstigate.com>
2024-07-22 18:02:34 +0200

    Fix the build when running inegration tests

--- core/bin/start.sh
+++ core/bin/start.sh
@@ -3,9 +3,5 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Run the fat jar
-#java -Dfile.encoding=UTF-8 -server -jar "$DIR/../target/restheart.jar" $@
+java -Dfile.encoding=UTF-8 -server -jar "$DIR/../target/restheart-core.jar" $@
 
-# run the regular jar with complete classpath
-java -Dfile.encoding=UTF-8 -server \
-    -cp "$DIR/../target/restheart-core.jar:$DIR/../target/lib/*:$DIR/../target/plugins/*" \
-    org.restheart.Bootstrapper $@
--- core/bin/stop.sh
+++ core/bin/stop.sh
@@ -3,9 +3,5 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # stop the fat jar
-#java -server -cp "$DIR/../target/restheart.jar" org.restheart.Shutdowner $@
+java -server -cp "$DIR/../target/restheart-core.jar" org.restheart.Shutdowner $@
 
-# stop the regular jar with complete classpath
-java -server \
-    -cp "$DIR/../target/restheart-core.jar:$DIR/../target/lib/*:$DIR/../target/plugins/*" \
-    org.restheart.Shutdowner $@
