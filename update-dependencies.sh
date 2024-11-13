5313cb5c196ed488d52a3367149a428d558383c8
Maurizio Turatti <maurizio@softinstigate.com>
2024-11-13 18:26:57 +0100

    :sparkles: Update update-dependencies.sh to include versions:update-properties for better dependency management

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -2,4 +2,4 @@
 # if the first argument is true, allow minor updates
 ALLOW_MINOR_UPDATES=${1:-false}
 echo "Updating dependencies with allowMinorUpdates=$ALLOW_MINOR_UPDATES"
-mvn versions:use-latest-releases -DallowMinorUpdates=$ALLOW_MINOR_UPDATES -DincludePlugins=true
+mvn versions:use-latest-releases versions:update-properties -DallowMinorUpdates=$ALLOW_MINOR_UPDATES -DincludePlugins=true
