3af0bad2898d0cd955e147b843a6ce242e494a02
Andrea Di Cesare <andrea@softinstigate.com>
2025-05-06 02:09:34 +0200

    :page_facing_up: Update license headers in all files

--- commons/src/main/java/org/restheart/utils/DirectoryWatcher.java
+++ commons/src/main/java/org/restheart/utils/DirectoryWatcher.java
@@ -2,7 +2,7 @@
  * ========================LICENSE_START=================================
  * restheart-polyglot
  * %%
- * Copyright (C) 2020 - 2024 SoftInstigate
+ * Copyright (C) 2020 - 2025 SoftInstigate
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
