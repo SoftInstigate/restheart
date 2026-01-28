0abe083056ebff555a72f1f9aeb5d632e6af2ef9
Andrea Di Cesare <andrea@softinstigate.com>
2026-01-28 16:21:01 +0100

    Update Copyright Year

--- graphql/src/main/java/org/restheart/graphql/predicates/FieldExists.java
+++ graphql/src/main/java/org/restheart/graphql/predicates/FieldExists.java
@@ -2,7 +2,7 @@
  * ========================LICENSE_START=================================
  * restheart-security
  * %%
- * Copyright (C) 2018 - 2025 SoftInstigate
+ * Copyright (C) 2018 - 2026 SoftInstigate
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
